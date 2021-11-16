7a89ae73a88d40f3d59c81065cb700af177671a4
shan-shaji <abrahamshanshaji@gmail.com>
2021-11-16 13:02:38 +0530

    test: add YaruRow test

---
+++ test/yaru_row_test.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:yaru_widgets/src/yaru_image_tile.dart';
+import 'package:yaru_widgets/src/yaru_option_button.dart';
+import 'package:yaru_widgets/src/yaru_row.dart';
+
+void main() {
+  testWidgets(
+      '- YaruRow Test when actionWidget && trailingWidget && leadingWidget != null',
+      (WidgetTester tester) async {
+    await tester.pumpWidget(MaterialApp(
+      theme: ThemeData(primarySwatch: Colors.red),
+      home: const Scaffold(
+        body: YaruRow(
+          leadingWidget: Text("Foo Leading"),
+          actionWidget: Text("Foo Text"),
+          trailingWidget: Icon(Icons.add),
+          description: "Foo Description",
+        ),
+      ),
+    ));
+
+    final trailingWidgetFinder = find.byWidgetPredicate(
+      (widget) => widget is Icon && widget.icon == Icons.add,
+    );
+    expect(find.text("Foo Text"), findsOneWidget);
+    expect(find.text("Foo Description"), findsOneWidget);
+    expect(find.text("Foo Leading"), findsOneWidget);
+    expect(trailingWidgetFinder, findsOneWidget);
+  });
+
+  testWidgets('- YaruRow widget when leadingWidget & Description == null',
+      (WidgetTester tester) async {
+    await tester.pumpWidget(MaterialApp(
+      theme: ThemeData(primarySwatch: Colors.red),
+      home: const Scaffold(
+        body: YaruRow(
+          leadingWidget: null,
+          actionWidget: Text("Foo Text"),
+          trailingWidget: Icon(Icons.add),
+        ),
+      ),
+    ));
+
+    final trailingWidgetFinder = find.byWidgetPredicate(
+      (widget) => widget is Icon && widget.icon == Icons.add,
+    );
+    expect(find.text("Foo Text"), findsOneWidget);
+    expect(find.text("Foo Description"), findsNothing);
+    expect(find.text("Foo Leading"), findsNothing);
+    expect(trailingWidgetFinder, findsOneWidget);
+  });
+}
