8d761d567446def28634e22f282a18dd7a8c2b8d
Paul Kepinski <paul.kepinski@orange.fr>
2021-12-07 23:09:34 +0100

    Fix infos messages

--- example/test/widget_test.dart
+++ example/test/widget_test.dart
@@ -8,8 +8,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:yaru_widgets_example/main.dart';
-
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
