8cc5f69bb4be1266aedda617e2c1c77377bb62c3
shan-shaji <abrahamshanshaji@gmail.com>
2021-12-07 12:59:08 +0530

    docs: add dart docs for YaruExtraOptionRow

--- lib/src/yaru_extra_option_row.dart
+++ lib/src/yaru_extra_option_row.dart
@@ -3,6 +3,7 @@ import 'package:yaru_widgets/src/yaru_option_button.dart';
 import 'package:yaru_widgets/src/yaru_row.dart';
 
 class YaruExtraOptionRow extends StatelessWidget {
+  /// Creates a row having switch, label, description and YaruOptionButton.
   const YaruExtraOptionRow({
     Key? key,
     required this.actionLabel,
@@ -13,11 +14,50 @@ class YaruExtraOptionRow extends StatelessWidget {
     required this.iconData,
   }) : super(key: key);
 
+  /// Specifies the label for the row.
   final String actionLabel;
+
+  /// Specifies description for the row.
   final String? actionDescription;
+
+  /// Current value of [Switch] in the row.
+  /// The value can be `true` or `false`
   final bool? value;
+
+  /// Called when the user toggles the switch on or off.
+  ///
+  /// The switch passes the new value to the callback but does not actually
+  /// change state until the parent widget rebuilds the switch with the new
+  /// value.
+  ///
+  /// The callback provided to [onChanged] should update the state of the parent
+  /// [StatefulWidget] using the [State.setState] method, so that the parent
+  /// gets rebuilt; for example:
+  ///
+  /// ```dart
+  ///
+  ///   bool _extraOptionValue = false;
+  ///   YaruExtraOptionRow(
+  ///     actionLabel: "Foo Label",
+  ///     iconData: YaruIcons.addon,
+  ///     onChanged: (c) {
+  ///     setState(() {
+  ///       _extraOptionValue = c;
+  ///       });
+  ///     },
+  ///     onPressed: () {},
+  ///     value: _extraOptionValue,
+  ///     actionDescription: "Foo Description",
+  ///   ),
+  ///
+  /// ```
   final Function(bool) onChanged;
+
+  /// Callback that needs to be passed for [YaruOptionButton].
+  /// This callback is triggered when the button is clicked.
   final VoidCallback onPressed;
+
+  /// IconData for the [YaruOptionButton].
   final IconData iconData;
 
   @override
