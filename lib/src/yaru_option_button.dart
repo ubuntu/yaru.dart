dc4d1fbc59fcf0d398b9f71fd5e3827f5a4e3a02
Shan Shaji <abrahamshanshaji@gmail.com>
2021-12-27 10:43:45 +0530

    docs: add dart docs (#3) (#25)
    * refactor: refactored code
    
    * docs: add dart docs
    
    * Add YaruMasterDetailsPage docs
    
    * docs: removed public docs
    
    * refactor: refactored code
    
    * docs: add YaruOptionButton docs
    
    * docs: add example
    
    * docs: add yaruPageContainer docs
    
    * fix: fix yaruPageContainer formatting
    
    * docs: add YaruRow docs
    
    * docs: add YaruSearchAppBar docs
    
    * docs: add more docs
    
    * docs: add YaruSection docs
    
    * docs: add YaruSingleInfo row docs
    
    * docs: add YaruSliderRow docs
    
    * fix: fix formatting issue
    
    * docs: add YaruSliderSecondary docs
    
    * docs: add YaruSwitchRow docs
    
    * docs: add YaruToggleButtonRow docs
    
    * docs: add YaruToggleButtonRow example in docs
    
    * docs: fix typo

--- lib/src/yaru_option_button.dart
+++ lib/src/yaru_option_button.dart
@@ -1,13 +1,26 @@
 import 'package:flutter/material.dart';
 
 class YaruOptionButton extends StatelessWidget {
+  /// Creates an [OutlinedButton] with Yaru theme.
+  /// The button have `height` and `width` of 40.
+  ///
+  /// for example:
+  /// ```dart
+  ///  YaruOptionButton(
+  ///           iconData: YaruIcons.search,
+  ///           onPressed: () {},
+  ///         ),
+  /// ```
   const YaruOptionButton({
     Key? key,
     required this.onPressed,
     required this.iconData,
   }) : super(key: key);
 
+  /// Callback that gets invoked when the button is clicked.
   final VoidCallback onPressed;
+
+  /// The [IconData] is place as a child of [OutlinedButton].
   final IconData iconData;
 
   @override
--- lib/src/yaru_page_container.dart
+++ lib/src/yaru_page_container.dart
@@ -1,12 +1,19 @@
 import 'package:flutter/material.dart';
 
 class YaruPageContainer extends StatelessWidget {
+  /// Creates a [Container] with specified `width` and `child`.
+  /// Total `width` of the [Container] will be `16 + width`.
+  /// If the given width is null default value will be 500.
   const YaruPageContainer({
     Key? key,
     required this.child,
     this.width,
   }) : super(key: key);
+
+  /// The child widget that is passed to the container.
   final Widget child;
+
+  /// Width of the [Container].
   final double? width;
 
   @override
