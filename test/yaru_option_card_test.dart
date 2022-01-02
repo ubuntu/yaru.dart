cc938135e55759047669ba83493e91a702a14edb
Shan Shaji <abrahamshanshaji@gmail.com>
2022-01-02 18:02:25 +0530

    test: add YaruOptionCard test (#35)

---
+++ test/yaru_option_card_test.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:yaru_widgets/src/yaru_option_card.dart';
+
+void main() {
+  testWidgets('- YaruOptionCard Test', (WidgetTester tester) async {
+    await tester.pumpWidget(MaterialApp(
+      theme: ThemeData(primarySwatch: Colors.red),
+      home: Scaffold(
+        body: YaruOptionCard(
+          selected: true,
+          onSelected: () {},
+          okIconData: Icons.analytics,
+          bodyText: "Foo Body",
+          titleText: "Foo Title",
+        ),
+      ),
+    ));
+
+    expect(find.byType(Card), findsOneWidget);
+    expect(find.byType(InkWell), findsOneWidget);
+    expect(find.text("Foo Body"), findsOneWidget);
+    expect(find.text("Foo Title"), findsOneWidget);
+  });
+}
