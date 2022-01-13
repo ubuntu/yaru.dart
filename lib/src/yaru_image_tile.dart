4406cc55480bf5ff781d9d48afc5a787ffdca559
shan-shaji <abrahamshanshaji@gmail.com>
2022-01-14 00:29:15 +0530

    feat: exposed filterquality property through ImageTile

--- lib/src/yaru_image_tile.dart
+++ lib/src/yaru_image_tile.dart
@@ -4,11 +4,7 @@ import 'package:flutter/material.dart';
 
 class ImageTile extends StatelessWidget {
   /// Creates a Image Tile from the image path given in the path property.
-  const ImageTile(
-      {Key? key,
-      required this.path,
-      this.onTap,
-      required this.currentlySelected})
+  const ImageTile({Key? key, required this.path, this.onTap, required this.currentlySelected, this.filterQuality})
       : super(key: key);
 
   // Specifies the path of the asset
@@ -23,6 +19,25 @@ class ImageTile extends StatelessWidget {
   /// Callback triggered when the [ImageTile] is clicked.
   final VoidCallback? onTap;
 
+  /// The rendering quality of the image.
+  ///
+  /// If the image is of a high quality and its pixels are perfectly aligned
+  /// with the physical screen pixels, extra quality enhancement may not be
+  /// necessary. If so, then [FilterQuality.none] would be the most efficient.
+  ///
+  /// If the pixels are not perfectly aligned with the screen pixels, or if the
+  /// image itself is of a low quality, [FilterQuality.none] may produce
+  /// undesirable artifacts. Consider using other [FilterQuality] values to
+  /// improve the rendered image quality in this case. Pixels may be misaligned
+  /// with the screen pixels as a result of transforms or scaling.
+  ///
+  /// See also:
+  ///
+  ///  * [FilterQuality], the enum containing all possible filter quality
+  ///    options.
+  ///  * By default [FilterQuality] will be [FilterQuality.low].
+  final FilterQuality? filterQuality;
+
   @override
   Widget build(BuildContext context) {
     return InkWell(
@@ -31,15 +46,12 @@ class ImageTile extends StatelessWidget {
       child: Container(
         decoration: BoxDecoration(
             borderRadius: BorderRadius.circular(8.0),
-            color: currentlySelected
-                ? Theme.of(context).primaryColor.withOpacity(0.3)
-                : Colors.transparent),
+            color: currentlySelected ? Theme.of(context).primaryColor.withOpacity(0.3) : Colors.transparent),
         child: Padding(
           padding: const EdgeInsets.all(6.0),
           child: ClipRRect(
             borderRadius: BorderRadius.circular(4),
-            child: Image.file(File(path),
-                filterQuality: FilterQuality.low, fit: BoxFit.fill),
+            child: Image.file(File(path), filterQuality: filterQuality ?? FilterQuality.low, fit: BoxFit.fill),
           ),
         ),
       ),
