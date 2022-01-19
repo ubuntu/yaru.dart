bbdffd4b5894b10d0c9ebd857d4daa573c88c329
shan-shaji <abrahamshanshaji@gmail.com>
2022-01-19 23:43:16 +0530

    test: add YaruPage test

---
+++ test/yaru_page_test.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:yaru_widgets/src/constants.dart';
+import 'package:yaru_widgets/src/yaru_option_button.dart';
+import 'package:yaru_widgets/yaru_widgets.dart';
+
+void main() {
+  testWidgets('YaruPage Test', (WidgetTester tester) async {
+    await tester.pumpWidget(MaterialApp(
+      home: Scaffold(
+        body: YaruPage(
+          child: Container(),
+          padding: const EdgeInsets.all(8),
+        ),
+      ),
+    ));
+
+    final paddingFinder = find.byWidgetPredicate(
+      (widget) =>
+          widget is Padding && widget.padding == const EdgeInsets.all(8),
+    );
+    expect(paddingFinder, findsOneWidget);
+    expect(find.byType(Container), findsOneWidget);
+  });
+
+  testWidgets('- Default padding will be given if padding is null',
+      (WidgetTester tester) async {
+    await tester.pumpWidget(MaterialApp(
+      home: Scaffold(
+        body: YaruPage(
+          child: Container(),
+        ),
+      ),
+    ));
+
+    final paddingFinder = find.byWidgetPredicate(
+      (widget) =>
+          widget is Padding && widget.padding == const EdgeInsets.all(kDefaultPagePadding),
+    );
+    expect(paddingFinder, findsOneWidget);
+    expect(find.byType(Container), findsOneWidget);
+  });
+}
