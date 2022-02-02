945abf240f9a67f7a8af987b34146443d3984295
Frederik Feichtmeier <frederik.feichtmeier@gmail.com>
2022-02-02 20:00:34 +0100

    YaruImageTile: rename and make it more flexible (#69)
    Rename YaruImageTile to YaruSelectableContainer with Improvements:
    
    - more flexible building with optional parameters
    - it now takes a widget and not an image

--- example/lib/yaru_home.dart
+++ example/lib/yaru_home.dart
@@ -1,3 +1,6 @@
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:yaru_icons/yaru_icons.dart';
 import 'package:yaru_widgets/yaru_widgets.dart';
@@ -17,6 +20,8 @@ class YaruHome extends StatefulWidget {
 class _YaruHomeState extends State<YaruHome> {
   bool _extraOptionValue = false;
   bool _isImageSelected = false;
+  bool _isOvalSelected = false;
+  bool _isTextSelected = false;
   final _textEditingController = TextEditingController();
   double _sliderValue = 0;
 
@@ -138,22 +143,44 @@ class _YaruHomeState extends State<YaruHome> {
         ),
       ),
       YaruPageItem(
-        title: 'ImageTile',
+        title: 'YaruSelectableContainer',
         iconData: YaruIcons.emote_devilish,
         builder: (_) => YaruPage(
           children: [
-            YaruImageTile(
-              currentlySelected: _isImageSelected,
-              onTap: () {
-                setState(() {
-                  if (_isImageSelected) {
-                    _isImageSelected = false;
-                  } else {
-                    _isImageSelected = true;
-                  }
-                });
-              },
-              path: "assets/ubuntuhero.jpg",
+            YaruSelectableContainer(
+              selected: _isImageSelected,
+              onTap: () => setState(() => _isImageSelected = !_isImageSelected),
+              child: kIsWeb
+                  ? Image.network('assets/ubuntuhero.jpg',
+                      filterQuality: FilterQuality.low, fit: BoxFit.fill)
+                  : Image.file(File('assets/ubuntuhero.jpg'),
+                      filterQuality: FilterQuality.low, fit: BoxFit.fill),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            YaruSelectableContainer(
+              selected: _isTextSelected,
+              onTap: () => setState(() => _isTextSelected = !_isTextSelected),
+              child: Padding(
+                padding: const EdgeInsets.all(18.0),
+                child: Text('This is just text but can be selected!'),
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            YaruSelectableContainer(
+              borderRadius: BorderRadius.circular(100.0),
+              selected: _isOvalSelected,
+              onTap: () => setState(() => _isOvalSelected = !_isOvalSelected),
+              child: ClipOval(
+                child: Material(
+                  color: Colors.amber, // Button color
+                  child: SizedBox(
+                      width: 56, height: 56, child: Icon(YaruIcons.heart)),
+                ),
+              ),
             )
           ],
         ),
---
+++ lib/src/yaru_selectable_container.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+class YaruSelectableContainer extends StatelessWidget {
+  /// Creates a Image Tile from the image path given in the path property.
+  const YaruSelectableContainer({
+    Key? key,
+    required this.child,
+    this.onTap,
+    required this.selected,
+    this.borderRadius,
+    this.radius = 6.0,
+    this.padding,
+    this.selectionColor,
+  }) : super(key: key);
+
+  // The child which will be selected with [onTap]
+  final Widget child;
+
+  /// Current Value of the imageTile.
+  /// Based on the this value selection of the image can be managed.
+  /// If this value is `true` [Container] border will have color from [Theme.of(context).primaryColor]
+  /// else if the value is `false` the border color will be [Colors.transparent].
+  final bool selected;
+
+  /// Callback triggered when the [YaruSelectableContainer] is clicked.
+  final VoidCallback? onTap;
+
+  /// Optional custom radius for the corners which defaults to a circular 6.0 [BorderRadius]
+  final BorderRadius? borderRadius;
+
+  /// Optional double value used for a symmetric circular [BorderRadius] if [borderRadius]
+  /// is not specified.
+  final double radius;
+
+  /// Optional custom padding for the child which defaults to 6.0 on all sides.
+  final EdgeInsets? padding;
+
+  /// Optional custom [Color] which is used for the selection border.
+  final Color? selectionColor;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      borderRadius: borderRadius ?? BorderRadius.circular(radius),
+      onTap: onTap,
+      child: Container(
+        decoration: BoxDecoration(
+            borderRadius: borderRadius ?? BorderRadius.circular(radius),
+            color: selected
+                ? selectionColor ??
+                    Theme.of(context).primaryColor.withOpacity(0.8)
+                : Colors.transparent),
+        child: Padding(
+          padding: padding ?? const EdgeInsets.all(6.0),
+          child: ClipRRect(
+            borderRadius:
+                borderRadius ?? BorderRadius.circular(radius - radius / 3),
+            child: child,
+          ),
+        ),
+      ),
+    );
+  }
+}
