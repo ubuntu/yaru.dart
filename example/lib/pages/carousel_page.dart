ba8f2caa3993ea879df33b4748b328fca4409668
Paul Kepinski <36476595+Jupi007@users.noreply.github.com>
2022-05-14 16:55:11 +0200

    Carousel improvements  (#123)
    * WIP
    
    * YaruCarousel improvemets
    
    * Remove useless space

--- example/lib/pages/carousel_page.dart
+++ example/lib/pages/carousel_page.dart
@@ -15,23 +15,19 @@ class _CarouselPageState extends State<CarouselPage> {
   @override
   Widget build(BuildContext context) {
     return YaruPage(children: [
-      YaruCarousel(
-        children: List.generate(
-          length,
-          (_) => Container(
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10),
-              border: Border.all(
-                  color:
-                      Theme.of(context).colorScheme.onSurface.withOpacity(0.1)),
-              image: DecorationImage(
-                  fit: BoxFit.contain,
-                  image: AssetImage('assets/ubuntuhero.jpg')),
-            ),
-          ),
+      YaruSection(headline: 'Auto scroll: off', width: 700, children: [
+        YaruCarousel(
+          children: _getCarouselChildren(),
+          height: 400,
         ),
-        height: 400,
-      ),
+      ]),
+      YaruSection(headline: 'Auto scroll: on', width: 700, children: [
+        YaruCarousel(
+          children: _getCarouselChildren(),
+          height: 400,
+          autoScroll: true,
+        ),
+      ]),
       YaruRow(
           width: 300,
           trailingWidget: Text('length: $length'),
@@ -54,5 +50,20 @@ class _CarouselPageState extends State<CarouselPage> {
           ),
           enabled: true)
     ]);
+  }
+
+  List<Widget> _getCarouselChildren() {
+    return List.generate(
+      length,
+      (_) => Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          border: Border.all(
+              color: Theme.of(context).colorScheme.onSurface.withOpacity(0.1)),
+          image: DecorationImage(
+              fit: BoxFit.contain, image: AssetImage('assets/ubuntuhero.jpg')),
+        ),
+      ),
+    );
   }
 }
--- lib/src/yaru_carousel.dart
+++ lib/src/yaru_carousel.dart
@@ -1,16 +1,19 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 
 const _kAnimationDuration = Duration(milliseconds: 500);
 const _kAnimationCurve = Curves.easeInOutCubic;
 
 class YaruCarousel extends StatefulWidget {
-  const YaruCarousel({
-    Key? key,
-    this.height = 500,
-    this.width = 500,
-    required this.children,
-    this.initialIndex = 0,
-  }) : super(key: key);
+  const YaruCarousel(
+      {Key? key,
+      this.height = 500,
+      this.width = 500,
+      required this.children,
+      this.initialIndex = 0,
+      this.autoScroll = false,
+      this.autoScrollDuration = const Duration(seconds: 1)})
+      : super(key: key);
 
   /// The height of the children, defaults to 500.0.
   final double height;
@@ -24,103 +27,143 @@ class YaruCarousel extends StatefulWidget {
   /// The index of the child that should be shown on first page load.
   final int initialIndex;
 
+  /// Enable an auto scrolling loop of all children
+  final bool autoScroll;
+
+  /// If [autoScroll] is enabled, this value determine the time spent on each carousel child
+  final Duration autoScrollDuration;
+
   @override
   State<YaruCarousel> createState() => _YaruCarouselState();
 }
 
 class _YaruCarouselState extends State<YaruCarousel> {
   late PageController _pageController;
-
+  late Timer _timer;
   late int _index;
 
   @override
   void initState() {
     super.initState();
+
     _index = widget.initialIndex;
     _pageController = PageController(
       viewportFraction: 0.8,
       initialPage: _index,
     );
+
+    if (widget.autoScroll) {
+      _startTimer();
+    }
+  }
+
+  @override
+  void didUpdateWidget(YaruCarousel oldWidget) {
+    super.didUpdateWidget(oldWidget);
+
+    if (_index > widget.children.length - 1) {
+      _animateToPage(widget.children.length - 1);
+    }
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+
+    if (widget.autoScroll) {
+      _cancelTimer();
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Column(
-      children: [
-        SizedBox(
-          height: widget.height,
-          width: widget.width,
-          child: PageView.builder(
-              itemCount: widget.children.length,
-              pageSnapping: true,
-              controller: _pageController,
-              onPageChanged: (index) => setState(() => _index = index),
-              itemBuilder: (context, index) => AnimatedContainer(
-                    duration: _kAnimationDuration,
-                    curve: _kAnimationCurve,
-                    margin: EdgeInsets.all(index == _index ? 10 : 20),
-                    child: _index == index
-                        ? widget.children[index]
-                        : GestureDetector(
-                            behavior: HitTestBehavior.opaque,
-                            onTap: () => _animateToPage(index),
-                            child: IgnorePointer(
-                              child: widget.children[index],
-                            ),
-                          ),
-                  )),
-        ),
-        widget.children.length < 30
-            ? SizedBox(
-                width: widget.children.length < 5
-                    ? widget.width / 3
-                    : widget.children.length < 10
-                        ? widget.width / 2
-                        : widget.width,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  mainAxisSize: MainAxisSize.min,
-                  children: List<Widget>.generate(
-                    widget.children.length,
-                    (index) => Expanded(
-                      child: GestureDetector(
-                        onTap: _index == index
-                            ? null
-                            : () => _animateToPage(index),
-                        child: Container(
-                          margin: const EdgeInsets.all(5),
-                          width: 12,
-                          height: 12,
-                          decoration: BoxDecoration(
-                              color: _index == index
-                                  ? Theme.of(context).colorScheme.primary
-                                  : Theme.of(context)
-                                      .colorScheme
-                                      .primary
-                                      .withOpacity(0.3),
-                              shape: BoxShape.circle),
+      children: [_buildCarousel(), _buildPlaceIndicator()],
+    );
+  }
+
+  Widget _buildCarousel() {
+    return SizedBox(
+      height: widget.height,
+      width: widget.width,
+      child: PageView.builder(
+          itemCount: widget.children.length,
+          pageSnapping: true,
+          controller: _pageController,
+          physics:
+              // Disable physic when auto scroll is enable because we cannot
+              // disable the timer when dragging the view
+              widget.autoScroll ? const NeverScrollableScrollPhysics() : null,
+          onPageChanged: (index) => setState(() => _index = index),
+          itemBuilder: (context, index) => AnimatedContainer(
+                duration: _kAnimationDuration,
+                curve: _kAnimationCurve,
+                margin: EdgeInsets.all(index == _index ? 10 : 20),
+                child: _index == index - 1 || _index == index + 1
+                    ? GestureDetector(
+                        behavior: HitTestBehavior.opaque,
+                        onTap: () => _animateToPage(index),
+                        child: IgnorePointer(
+                          child: widget.children[index],
                         ),
-                      ),
-                    ),
-                  ),
+                      )
+                    : widget.children[index],
+              )),
+    );
+  }
+
+  Widget _buildPlaceIndicator() {
+    // If children are lower than 30, return a dot based indicator
+    if (widget.children.length < 30) {
+      return SizedBox(
+        width: widget.children.length < 5
+            ? widget.width / 3
+            : widget.children.length < 10
+                ? widget.width / 2
+                : widget.width,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min,
+          children: List<Widget>.generate(
+            widget.children.length,
+            (index) => Expanded(
+              child: GestureDetector(
+                onTap: _index == index ? null : () => _animateToPage(index),
+                child: Container(
+                  margin: const EdgeInsets.all(5),
+                  width: 12,
+                  height: 12,
+                  decoration: BoxDecoration(
+                      color: _index == index
+                          ? Theme.of(context).colorScheme.primary
+                          : Theme.of(context)
+                              .colorScheme
+                              .primary
+                              .withOpacity(0.3),
+                      shape: BoxShape.circle),
                 ),
-              )
-            : SizedBox(
-                width: widget.width,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Text(
-                        '$_index/${widget.children.length}',
-                        style: Theme.of(context).textTheme.caption,
-                      ),
-                    )
-                  ],
-                ),
-              )
-      ],
+              ),
+            ),
+          ),
+        ),
+      );
+    }
+
+    // Else return a text based indicator
+    return SizedBox(
+      width: widget.width,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              '$_index/${widget.children.length}',
+              style: Theme.of(context).textTheme.caption,
+            ),
+          )
+        ],
+      ),
     );
   }
 
@@ -130,5 +173,20 @@ class _YaruCarouselState extends State<YaruCarousel> {
       duration: _kAnimationDuration,
       curve: _kAnimationCurve,
     );
+
+    if (widget.autoScroll) {
+      _cancelTimer();
+      _startTimer();
+    }
+  }
+
+  void _startTimer() {
+    _timer = Timer.periodic(widget.autoScrollDuration, (timer) {
+      _animateToPage(_index >= widget.children.length ? 0 : _index++);
+    });
+  }
+
+  void _cancelTimer() {
+    _timer.cancel();
   }
 }
