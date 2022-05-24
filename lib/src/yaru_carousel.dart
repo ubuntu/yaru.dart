3af9c93e8ad3e13f9c1ac66b8f3a66778cf68aa3
Paul Kepinski <paul.kepinski@orange.fr>
2022-05-24 15:20:32 +0200

    YaruCarousel: many improvements
    - Add animation on dot index change
    - Better responsive place indicator layout
    - Simplify text indicator code
    - Remove unconsistent margin - now reach each edge

--- lib/src/yaru_carousel.dart
+++ lib/src/yaru_carousel.dart
@@ -14,6 +14,7 @@ class YaruCarousel extends StatefulWidget {
       this.autoScroll = false,
       this.autoScrollDuration = const Duration(seconds: 1),
       this.placeIndicator = true,
+      this.placeIndicatorMarginTop = 12.0,
       this.viewportFraction = 0.8})
       : super(key: key);
 
@@ -37,9 +38,12 @@ class YaruCarousel extends StatefulWidget {
 
   /// Display a place indicator
   ///
-  /// Show a dot based indicator if children length is lower than 30,
+  /// Show a dot based indicator if there  is enough space,
   /// else use a text based indicator
   final bool placeIndicator;
+
+  /// Margin between the carousel and the place indicator
+  final double placeIndicatorMarginTop;
 
   /// The fraction of the viewport that each page should occupy.
   final double viewportFraction;
@@ -88,102 +92,109 @@ class _YaruCarouselState extends State<YaruCarousel> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        _buildCarousel(),
-        ...(widget.placeIndicator && widget.children.length > 1
-            ? [_buildPlaceIndicator()]
-            : [])
-      ],
-    );
+    return SizedBox(
+        height: widget.height,
+        width: widget.width,
+        child: Column(
+          children: [
+            _buildCarousel(),
+            ...(widget.placeIndicator && widget.children.length > 1
+                ? [
+                    SizedBox(
+                      height: widget.placeIndicatorMarginTop,
+                    ),
+                    _buildPlaceIndicator()
+                  ]
+                : [])
+          ],
+        ));
   }
 
   Widget _buildCarousel() {
-    return SizedBox(
-      height: widget.height,
-      width: widget.width,
-      child: PageView.builder(
-        itemCount: widget.children.length,
-        pageSnapping: true,
-        controller: _pageController,
-        physics:
-            // Disable physic when auto scroll is enable because we cannot
-            // disable the timer when dragging the view
-            widget.autoScroll ? const NeverScrollableScrollPhysics() : null,
-        onPageChanged: (index) => setState(() => _index = index),
-        itemBuilder: (context, index) => AnimatedScale(
-          scale: _index == index ? 1.0 : .95,
-          duration: _kAnimationDuration,
-          curve: _kAnimationCurve,
-          child: Container(
-            margin: const EdgeInsets.all(10),
-            child: _index == index - 1 || _index == index + 1
-                ? GestureDetector(
-                    behavior: HitTestBehavior.opaque,
-                    onTap: () => _animateToPage(index),
-                    child: IgnorePointer(
-                      child: widget.children[index],
-                    ),
-                  )
-                : widget.children[index],
+    return Expanded(
+        child: PageView.builder(
+      itemCount: widget.children.length,
+      pageSnapping: true,
+      controller: _pageController,
+      physics:
+          // Disable physic when auto scroll is enable because we cannot
+          // disable the timer when dragging the view
+          widget.autoScroll ? const NeverScrollableScrollPhysics() : null,
+      onPageChanged: (index) => setState(() => _index = index),
+      itemBuilder: (context, index) => AnimatedScale(
+        scale: _index == index ? 1.0 : .9,
+        duration: _kAnimationDuration,
+        curve: _kAnimationCurve,
+        child: Container(
+          child: _index == index - 1 || _index == index + 1
+              ? GestureDetector(
+                  behavior: HitTestBehavior.opaque,
+                  onTap: () => _animateToPage(index),
+                  child: IgnorePointer(
+                    child: widget.children[index],
+                  ),
+                )
+              : widget.children[index],
+        ),
+      ),
+    ));
+  }
+
+  Widget _buildPlaceIndicator() {
+    return LayoutBuilder(builder: (context, constraints) {
+      const double dotSize = 12.0;
+
+      for (var layout in [
+        [48.0, constraints.maxWidth / 2],
+        [24.0, constraints.maxWidth / 3 * 2],
+        [12.0, constraints.maxWidth / 6 * 5]
+      ]) {
+        final double dotSpacing = layout[0];
+        final double maxWidth = layout[1];
+
+        if (dotSize * widget.children.length +
+                dotSpacing * (widget.children.length - 1) <
+            maxWidth) {
+          return _buildDotIndicator(dotSize, dotSpacing);
+        }
+      }
+
+      return _buildTextIndicator();
+    });
+  }
+
+  Widget _buildDotIndicator(double dotSize, double dotSpacing) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      mainAxisSize: MainAxisSize.min,
+      children: List<Widget>.generate(
+        widget.children.length,
+        (index) => GestureDetector(
+          onTap: _index == index ? null : () => _animateToPage(index),
+          child: Padding(
+            padding: EdgeInsets.only(left: index != 0 ? dotSpacing : 0),
+            child: AnimatedContainer(
+              duration: _kAnimationDuration,
+              curve: _kAnimationCurve,
+              width: dotSize,
+              height: dotSize,
+              decoration: BoxDecoration(
+                  color: _index == index
+                      ? Theme.of(context).colorScheme.primary
+                      : Theme.of(context).colorScheme.primary.withOpacity(0.3),
+                  shape: BoxShape.circle),
+            ),
           ),
         ),
       ),
     );
   }
 
-  Widget _buildPlaceIndicator() {
-    // If children are lower than 30, return a dot based indicator
-    if (widget.children.length < 30) {
-      return SizedBox(
-        width: widget.children.length < 5
-            ? widget.width / 3
-            : widget.children.length < 10
-                ? widget.width / 2
-                : widget.width,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          mainAxisSize: MainAxisSize.min,
-          children: List<Widget>.generate(
-            widget.children.length,
-            (index) => Expanded(
-              child: GestureDetector(
-                onTap: _index == index ? null : () => _animateToPage(index),
-                child: Container(
-                  margin: const EdgeInsets.all(5),
-                  width: 12,
-                  height: 12,
-                  decoration: BoxDecoration(
-                      color: _index == index
-                          ? Theme.of(context).colorScheme.primary
-                          : Theme.of(context)
-                              .colorScheme
-                              .primary
-                              .withOpacity(0.3),
-                      shape: BoxShape.circle),
-                ),
-              ),
-            ),
-          ),
-        ),
-      );
-    }
-
-    // Else return a text based indicator
-    return SizedBox(
-      width: widget.width,
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Text(
-              '${_index + 1}/${widget.children.length}',
-              style: Theme.of(context).textTheme.caption,
-            ),
-          )
-        ],
-      ),
+  Widget _buildTextIndicator() {
+    return Text(
+      '${_index + 1}/${widget.children.length}',
+      style: Theme.of(context).textTheme.caption,
+      textAlign: TextAlign.center,
     );
   }
 
