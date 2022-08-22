8292f26728ad550ff7770dcbcecc030a672ba0f5
Paul Kepinski <paul.kepinski@orange.fr>
2022-08-22 22:42:04 +0200

    WIP

--- lib/src/animations/yaru_animated_ok_icon.dart
+++ lib/src/animations/yaru_animated_ok_icon.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
 class YaruAnimatedOkIcon extends StatefulWidget {
-  const YaruAnimatedOkIcon({super.key});
+  const YaruAnimatedOkIcon({this.size = 60.0, super.key});
+
+  final double size;
 
   @override
   _YaruAnimatedOkIconState createState() => _YaruAnimatedOkIconState();
@@ -17,7 +19,7 @@ class _YaruAnimatedOkIconState extends State<YaruAnimatedOkIcon>
     super.initState();
 
     _controller = AnimationController(
-      duration: const Duration(milliseconds: 1000),
+      duration: const Duration(milliseconds: 500),
       vsync: this,
     );
     _animation = Tween(begin: 0.0, end: 1.0).animate(_controller);
@@ -28,20 +30,28 @@ class _YaruAnimatedOkIconState extends State<YaruAnimatedOkIcon>
 
   @override
   void dispose() {
-    // Properly dispose the controller. This is important!
     _controller.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return AnimatedBuilder(
-      animation: _animation,
-      builder: ((context, child) {
-        return CustomPaint(
-          painter: _YaruAnimatedOkIconPainter(_animation.value),
-        );
-      }),
+    return ClipRRect(
+      child: SizedBox(
+        width: widget.size,
+        height: widget.size,
+        child: AnimatedBuilder(
+          animation: _animation,
+          builder: ((context, child) {
+            return CustomPaint(
+              painter: _YaruAnimatedOkIconPainter(
+                widget.size,
+                _animation.value,
+              ),
+            );
+          }),
+        ),
+      ),
     );
   }
 }
@@ -51,19 +61,32 @@ class _YaruAnimatedOkIconState extends State<YaruAnimatedOkIcon>
 // Draw checkmark during the whole animation
 
 class _YaruAnimatedOkIconPainter extends CustomPainter {
-  _YaruAnimatedOkIconPainter(this.animationPosition);
+  _YaruAnimatedOkIconPainter(this.size, this.animationPosition);
 
-  double animationPosition;
+  final double size;
+  final double animationPosition;
 
   @override
   void paint(Canvas canvas, Size size) {
-    canvas.drawCircle(
-      Offset(
-        size.width / 2,
-        size.height / 2,
-      ),
-      40.0 + 10.0 * animationPosition,
-      Paint()..color = Colors.black,
+    final checkMark = Path()
+      ..moveTo(size.width / 3, size.width / 2)
+      ..lineTo(size.width * 0.521, size.width * 0.688)
+      ..lineTo(size.width, size.width * 0.208);
+
+    final circle = Path()
+      ..addOval(
+        Rect.fromCircle(
+          center: Offset(size.width / 2, size.height / 2),
+          radius: size.width / 2 - 1,
+        ),
+      );
+
+    canvas.drawPath(
+      checkMark,
+      Paint()
+        ..color = Colors.black
+        ..style = PaintingStyle.stroke
+        ..strokeWidth = 2,
     );
   }
 
@@ -71,6 +94,7 @@ class _YaruAnimatedOkIconPainter extends CustomPainter {
   bool shouldRepaint(
     _YaruAnimatedOkIconPainter oldDelegate,
   ) {
-    return oldDelegate.animationPosition != animationPosition;
+    return oldDelegate.animationPosition != animationPosition ||
+        oldDelegate.size != size;
   }
 }
