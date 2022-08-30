7c1b4b094ee21dc1c2c63e3485815a09e0c6e897
Feichtmeier <frederik.feichtmeier@gmail.com>
2022-08-30 10:09:24 +0200

    Update example

--- example/lib/pages/tabbed_page_page.dart
+++ example/lib/pages/tabbed_page_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:yaru_icons/yaru_icons.dart';
 import 'package:yaru_widgets/yaru_widgets.dart';
-import 'package:yaru_widgets_example/widgets/row_list.dart';
 
 class TabbedPagePage extends StatefulWidget {
   const TabbedPagePage({
@@ -22,7 +21,27 @@ class _TabbedPagePageState extends State<TabbedPagePage> {
         initialIndex: _initialIndex,
         views: [
           YaruPage(
-            children: [RowList()],
+            children: [
+              GridView(
+                shrinkWrap: true,
+                gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+                  mainAxisExtent: 110,
+                  mainAxisSpacing: 15,
+                  crossAxisSpacing: 15,
+                  maxCrossAxisExtent: 550,
+                ),
+                children: [
+                  for (int i = 0; i < 20; i++)
+                    YaruBanner(
+                      name: 'YaruBanner $i',
+                      summary: 'Description',
+                      fallbackIconData: YaruIcons.ubuntu_logo_large,
+                      icon: Image.asset('assets/ubuntuhero.jpg'),
+                      onTap: () => showAboutDialog(context: context),
+                    )
+                ],
+              )
+            ],
           ),
           YaruPage(children: [
             YaruSection(
