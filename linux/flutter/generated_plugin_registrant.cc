148bdda998140eabd18213181b752aa91dd1513f
J-P Nurmi <jp.nurmi@canonical.com>
2022-09-17 11:46:26 +0200

    Master-detail: use the new page transitions in yaru.dart

--- linux/flutter/generated_plugin_registrant.cc
+++ linux/flutter/generated_plugin_registrant.cc
@@ -6,6 +6,10 @@
 
 #include "generated_plugin_registrant.h"
 
+#include <yaru/yaru_plugin.h>
 
 void fl_register_plugins(FlPluginRegistry* registry) {
+  g_autoptr(FlPluginRegistrar) yaru_registrar =
+      fl_plugin_registry_get_registrar_for_plugin(registry, "YaruPlugin");
+  yaru_plugin_register_with_registrar(yaru_registrar);
 }
