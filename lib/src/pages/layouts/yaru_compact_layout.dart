d2e45326deef0cf9b88e7a876080c8c7076e2ab3
Feichtmeier <frederik.feichtmeier@gmail.com>
2022-09-21 21:58:46 +0200

    Comact layout fixes
    - re-add scrollcontroller
    - add optional background color
    - readd contraints

--- lib/src/pages/layouts/yaru_compact_layout.dart
+++ lib/src/pages/layouts/yaru_compact_layout.dart
@@ -13,6 +13,7 @@ class YaruCompactLayout extends StatefulWidget {
     this.labelType = NavigationRailLabelType.none,
     this.extendNavigationRail = false,
     this.initialIndex = 0,
+    this.backgroundColor,
   }) : super(key: key);
 
   /// The list of [YaruPageItem] has to be provided.
@@ -34,6 +35,8 @@ class YaruCompactLayout extends StatefulWidget {
   /// The index of the [YaruPageItem] that is selected from [pageItems]
   final int initialIndex;
 
+  final Color? backgroundColor;
+
   @override
   State<YaruCompactLayout> createState() => _YaruCompactLayoutState();
 }
@@ -41,10 +44,19 @@ class YaruCompactLayout extends StatefulWidget {
 class _YaruCompactLayoutState extends State<YaruCompactLayout> {
   late int _index;
 
+  late ScrollController _controller;
+
   @override
   void initState() {
+    _controller = ScrollController();
     _index = widget.initialIndex;
     super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
   }
 
   @override
@@ -52,82 +64,99 @@ class _YaruCompactLayoutState extends State<YaruCompactLayout> {
     final unselectedTextColor =
         Theme.of(context).colorScheme.onSurface.withOpacity(0.8);
     final selectedTextColor = Theme.of(context).colorScheme.onSurface;
-    return SafeArea(
-      child: Scaffold(
-        body: Row(
-          mainAxisSize: MainAxisSize.min,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            SingleChildScrollView(
-              child: IntrinsicHeight(
-                child: NavigationRail(
-                  extended: widget.labelType == NavigationRailLabelType.none
-                      ? widget.extendNavigationRail
-                      : false,
-                  unselectedIconTheme: IconThemeData(
-                    color: unselectedTextColor,
-                  ),
-                  indicatorColor:
-                      Theme.of(context).colorScheme.onSurface.withOpacity(0.1),
-                  selectedIconTheme: IconThemeData(
-                    color: selectedTextColor,
-                  ),
-                  selectedLabelTextStyle: TextStyle(
-                    overflow: TextOverflow.ellipsis,
-                    color: selectedTextColor,
-                    fontSize: 13,
-                    fontWeight: FontWeight.w500,
-                  ),
-                  unselectedLabelTextStyle: TextStyle(
-                    color: unselectedTextColor,
-                    overflow: TextOverflow.ellipsis,
-                    fontSize: 13,
-                    fontWeight: FontWeight.w500,
-                  ),
-                  backgroundColor: Theme.of(context).colorScheme.background,
-                  selectedIndex: _index,
-                  onDestinationSelected: (index) {
-                    setState(() => _index = index);
-                  },
-                  labelType: widget.labelType,
-                  destinations: widget.pageItems
-                      .map(
-                        (pageItem) => NavigationRailDestination(
-                          icon: pageItem.itemWidget ?? Icon(pageItem.iconData),
-                          selectedIcon: pageItem.selectedItemWidget ??
-                              pageItem.itemWidget ??
-                              (pageItem.selectedIconData != null
-                                  ? Icon(pageItem.selectedIconData)
-                                  : Icon(pageItem.iconData)),
-                          label: pageItem.titleBuilder(context),
+    return LayoutBuilder(
+      builder: (context, constraint) {
+        return SafeArea(
+          child: Scaffold(
+            body: Row(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                SizedBox(
+                  height: MediaQuery.of(context).size.height,
+                  child: SingleChildScrollView(
+                    controller: _controller,
+                    child: ConstrainedBox(
+                      constraints:
+                          BoxConstraints(minHeight: constraint.maxHeight),
+                      child: IntrinsicHeight(
+                        child: NavigationRail(
+                          extended:
+                              widget.labelType == NavigationRailLabelType.none
+                                  ? widget.extendNavigationRail
+                                  : false,
+                          unselectedIconTheme: IconThemeData(
+                            color: unselectedTextColor,
+                          ),
+                          indicatorColor: Theme.of(context)
+                              .colorScheme
+                              .onSurface
+                              .withOpacity(0.1),
+                          selectedIconTheme: IconThemeData(
+                            color: selectedTextColor,
+                          ),
+                          selectedLabelTextStyle: TextStyle(
+                            overflow: TextOverflow.ellipsis,
+                            color: selectedTextColor,
+                            fontSize: 13,
+                            fontWeight: FontWeight.w500,
+                          ),
+                          unselectedLabelTextStyle: TextStyle(
+                            color: unselectedTextColor,
+                            overflow: TextOverflow.ellipsis,
+                            fontSize: 13,
+                            fontWeight: FontWeight.w500,
+                          ),
+                          backgroundColor: widget.backgroundColor ??
+                              Theme.of(context).colorScheme.background,
+                          selectedIndex: _index,
+                          onDestinationSelected: (index) {
+                            setState(() => _index = index);
+                          },
+                          labelType: widget.labelType,
+                          destinations: widget.pageItems
+                              .map(
+                                (pageItem) => NavigationRailDestination(
+                                  icon: pageItem.itemWidget ??
+                                      Icon(pageItem.iconData),
+                                  selectedIcon: pageItem.selectedItemWidget ??
+                                      pageItem.itemWidget ??
+                                      (pageItem.selectedIconData != null
+                                          ? Icon(pageItem.selectedIconData)
+                                          : Icon(pageItem.iconData)),
+                                  label: pageItem.titleBuilder(context),
+                                ),
+                              )
+                              .toList(),
                         ),
-                      )
-                      .toList(),
-                ),
-              ),
-            ),
-            const VerticalDivider(thickness: 1, width: 1),
-            Expanded(
-              child: Theme(
-                data: Theme.of(context).copyWith(
-                  pageTransitionsTheme: YaruPageTransitionsTheme.vertical,
-                ),
-                child: Navigator(
-                  pages: [
-                    MaterialPage(
-                      key: ValueKey(_index),
-                      child: widget.pageItems.length > _index
-                          ? widget.pageItems[_index].builder(context)
-                          : widget.pageItems[0].builder(context),
+                      ),
                     ),
-                  ],
-                  onPopPage: (route, result) => route.didPop(result),
+                  ),
                 ),
-              ),
-            )
-          ],
-        ),
-      ),
+                const VerticalDivider(thickness: 1, width: 1),
+                Expanded(
+                  child: Theme(
+                    data: Theme.of(context).copyWith(
+                      pageTransitionsTheme: YaruPageTransitionsTheme.vertical,
+                    ),
+                    child: Navigator(
+                      pages: [
+                        MaterialPage(
+                          key: ValueKey(_index),
+                          child: widget.pageItems.length > _index
+                              ? widget.pageItems[_index].builder(context)
+                              : widget.pageItems[0].builder(context),
+                        ),
+                      ],
+                      onPopPage: (route, result) => route.didPop(result),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+        );
+      },
     );
   }
 }
