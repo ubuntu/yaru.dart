3880fc0b5247037b3545c78d36a960e86e648f08
J-P Nurmi <jp.nurmi@canonical.com>
2022-10-05 22:37:02 +0200

    YaruTile: test LTR & RTL layout

--- test/pages/yaru_tile_test.dart
+++ test/pages/yaru_tile_test.dart
@@ -3,53 +3,94 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:yaru_widgets/src/pages/yaru_tile.dart';
 
 void main() {
-  testWidgets(
-      '- YaruTile Test when actionWidget && trailingWidget && leadingWidget != null',
-      (WidgetTester tester) async {
+  testWidgets('ltr layout', (tester) async {
     await tester.pumpWidget(
-      MaterialApp(
-        theme: ThemeData(primarySwatch: Colors.red),
-        home: const Scaffold(
+      const MaterialApp(
+        home: Scaffold(
           body: YaruTile(
-            leading: Text('Foo Leading'),
-            trailing: Text('Foo Text'),
-            title: Icon(Icons.add),
-            subtitle: Text('Foo Description'),
+            leading: Text('Leading'),
+            title: Text('Title'),
+            subtitle: Text('Subtitle'),
+            trailing: Text('Trailing'),
           ),
         ),
       ),
     );
 
-    final trailingWidgetFinder = find.byWidgetPredicate(
-      (widget) => widget is Icon && widget.icon == Icons.add,
+    final leading = find.text('Leading');
+    expect(leading, findsOneWidget);
+
+    final title = find.text('Title');
+    expect(title, findsOneWidget);
+
+    final subtitle = find.text('Subtitle');
+    expect(subtitle, findsOneWidget);
+
+    final trailing = find.text('Trailing');
+    expect(trailing, findsOneWidget);
+
+    expect(
+      tester.getRect(leading).right,
+      lessThan(tester.getRect(title).left),
     );
-    expect(find.text('Foo Text'), findsOneWidget);
-    expect(find.text('Foo Description'), findsOneWidget);
-    expect(find.text('Foo Leading'), findsOneWidget);
-    expect(trailingWidgetFinder, findsOneWidget);
+    expect(
+      tester.getRect(title).left,
+      equals(tester.getRect(subtitle).left),
+    );
+    expect(
+      tester.getRect(subtitle).top,
+      greaterThan(tester.getRect(title).bottom),
+    );
+    expect(
+      tester.getRect(trailing).left,
+      greaterThan(tester.getRect(title).right),
+    );
   });
 
-  testWidgets('- YaruTile widget when leadingWidget & Description == null',
-      (WidgetTester tester) async {
+  testWidgets('rtl layout', (tester) async {
     await tester.pumpWidget(
-      MaterialApp(
-        theme: ThemeData(primarySwatch: Colors.red),
-        home: const Scaffold(
-          body: YaruTile(
-            leading: null,
-            trailing: Text('Foo Text'),
-            title: Icon(Icons.add),
+      const MaterialApp(
+        home: Scaffold(
+          body: Directionality(
+            textDirection: TextDirection.rtl,
+            child: YaruTile(
+              leading: Text('Leading'),
+              title: Text('Title'),
+              subtitle: Text('Subtitle'),
+              trailing: Text('Trailing'),
+            ),
           ),
         ),
       ),
     );
 
-    final trailingWidgetFinder = find.byWidgetPredicate(
-      (widget) => widget is Icon && widget.icon == Icons.add,
+    final leading = find.text('Leading');
+    expect(leading, findsOneWidget);
+
+    final title = find.text('Title');
+    expect(title, findsOneWidget);
+
+    final subtitle = find.text('Subtitle');
+    expect(subtitle, findsOneWidget);
+
+    final trailing = find.text('Trailing');
+    expect(trailing, findsOneWidget);
+
+    expect(
+      tester.getRect(leading).left,
+      greaterThan(tester.getRect(title).right),
     );
-    expect(find.text('Foo Text'), findsOneWidget);
-    expect(find.text('Foo Description'), findsNothing);
-    expect(find.text('Foo Leading'), findsNothing);
-    expect(trailingWidgetFinder, findsOneWidget);
+    expect(
+      tester.getRect(title).right,
+      equals(tester.getRect(subtitle).right),
+    );
+    expect(
+      tester.getRect(subtitle).top,
+      greaterThan(tester.getRect(title).bottom),
+    );
+    expect(
+      tester.getRect(trailing).right,
+      lessThan(tester.getRect(title).left),
+    );
   });
 }
