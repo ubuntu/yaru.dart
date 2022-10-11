d6a01a7483cd2ab62afa7c2db205b613490e9d24
Paul Kepinski <paul.kepinski@orange.fr>
2022-10-11 16:46:23 +0200

    YaruLandscapeLayout: use DividerTheme (#283)
    Use DividerTheme instead of hardcoded color to be aligned with the compact layout.

--- lib/src/pages/layouts/yaru_landscape_layout.dart
+++ lib/src/pages/layouts/yaru_landscape_layout.dart
@@ -119,8 +119,6 @@ class _YaruLandscapeLayoutState extends State<YaruLandscapeLayout> {
     );
   }
 
-  Color get _separatorColor => Colors.black.withOpacity(0.1);
-
   Widget _maybeBuildGlobalMouseRegion(Widget child) {
     if (widget.allowLeftPaneResize) {
       return MouseRegion(
@@ -150,10 +148,9 @@ class _YaruLandscapeLayoutState extends State<YaruLandscapeLayout> {
   }
 
   Widget _buildVerticalSeparator() {
-    return VerticalDivider(
+    return const VerticalDivider(
       thickness: 1,
       width: 1,
-      color: _separatorColor,
     );
   }
 
@@ -188,8 +185,9 @@ class _YaruLandscapeLayoutState extends State<YaruLandscapeLayout> {
     return Positioned(
       child: AnimatedContainer(
         duration: _kLeftPaneResizingRegionAnimationDuration,
-        color:
-            _isHovering || _isDragging ? _separatorColor : Colors.transparent,
+        color: _isHovering || _isDragging
+            ? DividerTheme.of(context).color
+            : Colors.transparent,
         child: MouseRegion(
           cursor: SystemMouseCursors.resizeColumn,
           onEnter: (event) => setState(() {
