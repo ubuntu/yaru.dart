76d77f36c1bdeacc1e5011dc28889a98fb20a256
J-P Nurmi <jp.nurmi@canonical.com>
2022-10-23 10:19:25 +0200

    Document and clean up the constants

--- lib/src/constants.dart
+++ lib/src/constants.dart
@@ -1,7 +1,15 @@
+/// The recommended padding around page content in Yaru applications.
 const kYaruPagePadding = 20.0;
-const kYaruDialogTitleHeight = 72.0;
-const kYaruDialogTitlePadding = 0.0;
-const kYaruPageWidth = 500.0;
+
+/// The default height of [YaruTitleBar].
+const kYaruTitleBarHeight = 72.0;
+
+/// The default border radius for Yaru-style containers.
 const kYaruContainerRadius = 8.0;
+
+/// The default border radius for Yaru-style buttons.
 const kYaruButtonRadius = 6.0;
+
+/// The default breakpoint width [YaruMasterDetailPage] uses for switching
+/// between portrait and landscape modes.
 const kYaruMasterDetailBreakpoint = 620.0;
