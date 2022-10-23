143372490179d647827ee64febce0f64a2d251dc
J-P Nurmi <jp.nurmi@canonical.com>
2022-10-23 21:50:36 +0200

    YaruCloseButton: add golden test (#324)

---
+++ test/controls/yaru_close_button_test.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:yaru_widgets/yaru_widgets.dart';
+
+import '../yaru_golden_tester.dart';
+
+void main() {
+  testWidgets(
+    'golden images',
+    (tester) async {
+      final variant = goldenVariant.currentValue!;
+
+      await tester.pumpScaffold(
+        const YaruCloseButton(),
+        themeMode: variant.themeMode,
+        size: const Size(40, 40),
+      );
+      await tester.pumpAndSettle();
+
+      if (variant.hasState(MaterialState.pressed)) {
+        await tester.down(find.byType(YaruCloseButton));
+        await tester.pumpAndSettle();
+      } else if (variant.hasState(MaterialState.hovered)) {
+        await tester.hover(find.byType(YaruCloseButton));
+        await tester.pumpAndSettle();
+      }
+
+      await expectLater(
+        find.byType(YaruCloseButton),
+        matchesGoldenFile('goldens/yaru_close_button-${variant.label}.png'),
+      );
+    },
+    variant: goldenVariant,
+    tags: 'golden',
+  );
+}
+
+final goldenVariant = ValueVariant({
+  ...goldenThemeVariants('normal'),
+  ...goldenThemeVariants('hovered', {MaterialState.hovered: true}),
+  ...goldenThemeVariants('pressed', {MaterialState.pressed: true}),
+});
