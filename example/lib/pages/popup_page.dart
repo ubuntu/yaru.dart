f381a2d2968cc072803fae2d1148a05e1639afc7
Frederik Feichtmeier <frederik.feichtmeier@gmail.com>
2022-10-23 17:34:35 +0200

    Example: clean up popup page (#321)

--- example/lib/pages/popup_page.dart
+++ example/lib/pages/popup_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_markdown/flutter_markdown.dart';
 import 'package:yaru_widgets/yaru_widgets.dart';
 
 class PopupPage extends StatefulWidget {
@@ -15,105 +14,75 @@ class _PopupPageState extends State<PopupPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Expanded(
-          child: Padding(
-            padding: const EdgeInsets.all(kYaruPagePadding),
-            child: Wrap(
-              spacing: 10,
-              runSpacing: 10,
-              children: [
-                YaruPopupMenuButton<MyEnum>(
-                  initialValue: myEnum,
-                  onSelected: (v) {
-                    setState(() {
-                      myEnum = v;
-                    });
-                  },
-                  child: Text(myEnum.name),
-                  itemBuilder: (context) {
-                    return [
-                      for (final value in MyEnum.values)
-                        PopupMenuItem(
-                          value: value,
-                          child: Text(
-                            value.name,
-                          ),
-                        )
-                    ];
-                  },
-                ),
-                YaruPopupMenuButton<MyEnum>(
-                  onSelected: (value) {
-                    if (enumSet.contains(value)) {
-                      enumSet.remove(value);
-                    } else {
-                      enumSet.add(value);
-                    }
-                  },
-                  child: const Text('Multi Select'),
-                  itemBuilder: (context) {
-                    return [
-                      for (final value in MyEnum.values)
-                        YaruCheckedPopupMenuItem<MyEnum>(
-                          padding: EdgeInsets.zero,
-                          value: value,
-                          checked: enumSet.contains(value),
-                          child: Text(value.name),
-                        )
-                    ];
-                  },
-                ),
-                YaruPopupMenuButton<MyEnum>(
-                  child: const Text('Multi Select Without close'),
-                  itemBuilder: (context) {
-                    return [
-                      for (final value in MyEnum.values)
-                        YaruMultiSelectPopupMenuItem<MyEnum>(
-                          padding: EdgeInsets.zero,
-                          value: value,
-                          child: Text(value.name),
-                          checked: enumSet.contains(value),
-                          onChanged: (checked) {
-                            // Handle model changes here
-                            setState(() {
-                              checked
-                                  ? enumSet.add(value)
-                                  : enumSet.remove(value);
-                            });
-                          },
-                        )
-                    ];
-                  },
-                )
-              ],
-            ),
+    return Padding(
+      padding: const EdgeInsets.all(kYaruPagePadding),
+      child: Wrap(
+        spacing: 10,
+        runSpacing: 10,
+        children: [
+          YaruPopupMenuButton<MyEnum>(
+            initialValue: myEnum,
+            onSelected: (v) {
+              setState(() {
+                myEnum = v;
+              });
+            },
+            child: Text(myEnum.name),
+            itemBuilder: (context) {
+              return [
+                for (final value in MyEnum.values)
+                  PopupMenuItem(
+                    value: value,
+                    child: Text(
+                      value.name,
+                    ),
+                  )
+              ];
+            },
           ),
-        ),
-        const SizedBox(
-          height: 500,
-          width: 500,
-          child: Markdown(
-            data: '''                  
+          YaruPopupMenuButton<MyEnum>(
+            onSelected: (value) {
+              if (enumSet.contains(value)) {
+                enumSet.remove(value);
+              } else {
+                enumSet.add(value);
+              }
+            },
+            child: const Text('Multi Select'),
+            itemBuilder: (context) {
+              return [
+                for (final value in MyEnum.values)
+                  YaruCheckedPopupMenuItem<MyEnum>(
+                    padding: EdgeInsets.zero,
+                    value: value,
+                    checked: enumSet.contains(value),
+                    child: Text(value.name),
+                  )
+              ];
+            },
+          ),
+          YaruPopupMenuButton<MyEnum>(
+            child: const Text('Multi Select Without close'),
+            itemBuilder: (context) {
+              return [
+                for (final value in MyEnum.values)
                   YaruMultiSelectPopupMenuItem<MyEnum>(
-                          padding: EdgeInsets.zero,
-                          value: value,
-                          child: Text(value.name),
-                          checked: enumSet.contains(value),
-                          onChanged: (checked) {
-                            // Handle model changes here
-                            setState(() {
-                              checked
-                                  ? enumSet.add(value)
-                                  : enumSet.remove(value);
-                            });
-                          },
-                        )
-                  ''',
-          ),
-        ),
-      ],
+                    padding: EdgeInsets.zero,
+                    value: value,
+                    child: Text(value.name),
+                    checked: enumSet.contains(value),
+                    onChanged: (checked) {
+                      // Handle model changes here
+                      setState(() {
+                        checked ? enumSet.add(value) : enumSet.remove(value);
+                      });
+                    },
+                  )
+              ];
+            },
+          )
+        ],
+      ),
     );
   }
 }
