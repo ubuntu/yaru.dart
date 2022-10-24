a7187c77650c0e727fd36de71ce08d7acb8d1142
J-P Nurmi <jp.nurmi@canonical.com>
2022-10-24 13:09:14 +0200

    Add YaruBorderContainer
    This is sort of a fusion the software app's BorderContainer and the
    installer's RoundedContainer.

---
+++ lib/src/utilities/yaru_border_container.dart
@@ -0,0 +1,107 @@
+import 'package:flutter/material.dart';
+
+import '../constants.dart';
+
+/// A container with a rounded Yaru-style border.
+class YaruBorderContainer extends StatelessWidget {
+  /// Creates a [YaruBorderContainer].
+  const YaruBorderContainer({
+    super.key,
+    this.alignment,
+    this.padding,
+    this.color,
+    this.width,
+    this.height,
+    this.constraints,
+    this.margin,
+    this.transform,
+    this.transformAlignment,
+    this.child,
+    this.clipBehavior = Clip.none,
+    this.border,
+    this.borderRadius,
+  });
+
+  /// See [Container.child].
+  final Widget? child;
+
+  /// See [Container.alignment].
+  final AlignmentGeometry? alignment;
+
+  /// See [Container.padding].
+  final EdgeInsets? padding;
+
+  /// See [Container.color].
+  final Color? color;
+
+  /// See [Container.width].
+  final double? width;
+
+  /// See [Container.height].
+  final double? height;
+
+  /// See [Container.constraints].
+  final BoxConstraints? constraints;
+
+  /// See [Container.margin].
+  final EdgeInsetsGeometry? margin;
+
+  /// See [Container.transform].
+  final Matrix4? transform;
+
+  /// See [Container.transformAlignment].
+  final AlignmentGeometry? transformAlignment;
+
+  /// See [Container.clipBehavior].
+  final Clip clipBehavior;
+
+  /// The border.
+  ///
+  /// The default border is 1px wide and the color is [DividerThemeData.color]
+  /// or [ThemeData.dividerColor] if the former is null.
+  final BoxBorder? border;
+
+  /// The border radius.
+  ///
+  /// The default border is circular with the radius of `kYaruContainerRadius`.
+  final BorderRadiusGeometry? borderRadius;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final effectiveBorderRadius =
+        borderRadius ?? BorderRadius.circular(kYaruContainerRadius);
+    final effectiveBorder = border ??
+        Border.all(color: DividerTheme.of(context).color ?? theme.dividerColor);
+    final effectiveColor = color ??
+        (theme.brightness == Brightness.light
+            ? Colors.white
+            : theme.colorScheme.onSurface.withOpacity(0.03));
+
+    return Container(
+      alignment: alignment,
+      constraints: constraints,
+      margin: margin,
+      transform: transform,
+      transformAlignment: transformAlignment,
+      clipBehavior: clipBehavior,
+      padding: padding,
+      height: height,
+      width: width,
+      foregroundDecoration: BoxDecoration(
+        border: effectiveBorder,
+        borderRadius: effectiveBorderRadius,
+      ),
+      decoration: BoxDecoration(
+        color: effectiveColor,
+        borderRadius: effectiveBorderRadius,
+      ),
+      child: Material(
+        clipBehavior: clipBehavior,
+        color: Colors.transparent,
+        borderRadius: effectiveBorderRadius,
+        child: child,
+      ),
+    );
+  }
+}
