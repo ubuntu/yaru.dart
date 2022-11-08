03c77b4a9cd397d4318dacf75b45463af5bf26dd
Paul Kepinski <paul.kepinski@orange.fr>
2022-11-08 13:44:48 +0100

    Use YaruSwitch in example (#364)

--- example/lib/pages/controls_page.dart
+++ example/lib/pages/controls_page.dart
@@ -64,7 +64,7 @@ class _ControlsPageState extends State<ControlsPage> {
                 onChanged: (v) => setState(() => _switchValues[i] = v),
               ),
               const SizedBox(width: 10),
-              Switch(
+              YaruSwitch(
                 value: _switchValues[i],
                 onChanged: null,
               )
