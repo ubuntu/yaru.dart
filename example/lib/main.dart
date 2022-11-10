ee7849d519389a810bbca724253cc1e2c4edba8b
Frederik Feichtmeier <frederik.feichtmeier@gmail.com>
2022-11-10 12:37:56 +0100

    Center the code-snippet button (#371)

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -96,65 +96,71 @@ class _HomeState extends State<Home> {
       return Container();
     }
 
-    return IconButton(
-      onPressed: () => showDialog(
-        barrierDismissible: true,
-        context: context,
-        builder: (context) {
-          var snippet = '';
+    return Padding(
+      padding: const EdgeInsets.only(right: 8),
+      child: Center(
+        child: YaruIconButton(
+          onPressed: () => showDialog(
+            barrierDismissible: true,
+            context: context,
+            builder: (context) {
+              var snippet = '';
 
-          return AlertDialog(
-            titlePadding: EdgeInsets.zero,
-            title: YaruTitleBar(
-              title: Text(pageItem.tooltipMessage),
-              trailing: const YaruCloseButton(),
-              leading: IconButton(
-                icon: const Icon(YaruIcons.edit_copy),
-                tooltip: 'Copy',
-                onPressed: () async {
-                  await Clipboard.setData(
-                    ClipboardData(text: snippet),
-                  );
-                },
-              ),
-            ),
-            content: FutureBuilder<String>(
-              future: _getCodeSnippet(
-                pageItem.snippetUrl!,
-              ),
-              builder: (context, snapshot) {
-                switch (snapshot.connectionState) {
-                  case ConnectionState.none:
-                  case ConnectionState.waiting:
-                  case ConnectionState.active:
-                    return const Center(
-                      child: YaruCircularProgressIndicator(
-                        strokeWidth: 3,
-                      ),
-                    );
-                  case ConnectionState.done:
-                    snippet = snapshot.data!;
-                    return SingleChildScrollView(
-                      child: HighlightView(
-                        snippet,
-                        language: 'dart',
-                        theme: Theme.of(context).brightness == Brightness.dark
-                            ? vs2015Theme
-                            : vsTheme,
-                        padding: const EdgeInsets.all(12),
-                        textStyle: const TextStyle(
-                          fontSize: 16,
-                        ),
-                      ),
-                    );
-                }
-              },
-            ),
-          );
-        },
+              return AlertDialog(
+                titlePadding: EdgeInsets.zero,
+                title: YaruTitleBar(
+                  title: Text(pageItem.tooltipMessage),
+                  trailing: const YaruCloseButton(),
+                  leading: IconButton(
+                    icon: const Icon(YaruIcons.edit_copy),
+                    tooltip: 'Copy',
+                    onPressed: () async {
+                      await Clipboard.setData(
+                        ClipboardData(text: snippet),
+                      );
+                    },
+                  ),
+                ),
+                content: FutureBuilder<String>(
+                  future: _getCodeSnippet(
+                    pageItem.snippetUrl!,
+                  ),
+                  builder: (context, snapshot) {
+                    switch (snapshot.connectionState) {
+                      case ConnectionState.none:
+                      case ConnectionState.waiting:
+                      case ConnectionState.active:
+                        return const Center(
+                          child: YaruCircularProgressIndicator(
+                            strokeWidth: 3,
+                          ),
+                        );
+                      case ConnectionState.done:
+                        snippet = snapshot.data!;
+                        return SingleChildScrollView(
+                          child: HighlightView(
+                            snippet,
+                            language: 'dart',
+                            theme:
+                                Theme.of(context).brightness == Brightness.dark
+                                    ? vs2015Theme
+                                    : vsTheme,
+                            padding: const EdgeInsets.all(12),
+                            textStyle: const TextStyle(
+                              fontSize: 16,
+                            ),
+                          ),
+                        );
+                    }
+                  },
+                ),
+              );
+            },
+          ),
+          icon: const Icon(YaruIcons.desktop_panel_look),
+          tooltip: 'Example snippet',
+        ),
       ),
-      icon: const Icon(YaruIcons.desktop_panel_look),
-      tooltip: 'Example snippet',
     );
   }
 }
