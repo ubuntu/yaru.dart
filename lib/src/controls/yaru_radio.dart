f075a16e1a915bd7838ab198578793b1dabadb47
Paul Kepinski <paul.kepinski@orange.fr>
2022-11-11 21:12:38 +0100

    Add comments on new controls widgets Fixes #374

--- lib/src/controls/yaru_radio.dart
+++ lib/src/controls/yaru_radio.dart
@@ -1,27 +1,122 @@
 import 'package:flutter/material.dart';
 
 import '../constants.dart';
+import 'yaru_checkbox.dart';
+import 'yaru_radio_button.dart';
+import 'yaru_switch.dart';
 import 'yaru_togglable.dart';
 
 const _kDotSizeFactor = 0.4;
 
-class YaruRadio<T> extends YaruTogglable<T?> {
+/// A Yaru radio.
+///
+/// Used to select between a number of mutually exclusive values. When one radio
+/// button in a group is selected, the other radio buttons in the group cease to
+/// be selected. The values are of type [T], the type parameter of the [YaruRadio]
+/// class. Enums are commonly used for this purpose.
+///
+/// The radio button itself does not maintain any state. Instead, selecting the
+/// radio invokes the [onChanged] callback, passing [value] as a parameter. If
+/// [groupValue] and [value] match, this radio will be selected. Most widgets
+/// will respond to [onChanged] by calling [State.setState] to update the
+/// radio button's [groupValue].
+///
+/// See also:
+///
+///  * [YaruRadioButton], a desktop style radio button with an interactive label.
+///  * [Slider], for selecting a value in a range.
+///  * [YaruCheckbox] and [YaruSwitch], for toggling a particular value on or off.
+class YaruRadio<T> extends StatefulWidget implements YaruTogglable<T?> {
+  /// Create a Yaru radio.
+  ///
+  /// The radio button itself does not maintain any state. Instead, selecting the
+  /// radio invokes the [onChanged] callback, passing [value] as a parameter. If
+  /// [groupValue] and [value] match, this radio will be selected. Most widgets
+  /// will respond to [onChanged] by calling [State.setState] to update the
+  /// radio button's [groupValue].
   const YaruRadio({
     super.key,
-    required super.value,
+    required this.value,
     required this.groupValue,
     this.toggleable = false,
-    required super.onChanged,
-    super.focusNode,
-    super.autofocus,
+    required this.onChanged,
+    this.focusNode,
+    this.autofocus = false,
   }) : assert(toggleable || value != null);
 
-  final bool toggleable;
+  /// The value represented by this radio button.
+  @override
+  final T value;
+
+  /// The currently selected value for a group of radio buttons.
+  ///
+  /// This radio button is considered selected if its [value] matches the
+  /// [groupValue].
+  final T? groupValue;
 
   @override
   bool get checked => value == groupValue;
 
-  final T? groupValue;
+  /// Set to true if this radio button is allowed to be returned to an
+  /// indeterminate state by selecting it again when selected.
+  ///
+  /// To indicate returning to an indeterminate state, [onChanged] will be
+  /// called with null.
+  ///
+  /// If true, [onChanged] can be called with [value] when selected while
+  /// [groupValue] != [value], or with null when selected again while
+  /// [groupValue] == [value].
+  ///
+  /// If false, [onChanged] will be called with [value] when it is selected
+  /// while [groupValue] != [value], and only by selecting another radio button
+  /// in the group (i.e. changing the value of [groupValue]) can this radio
+  /// button be unselected.
+  ///
+  /// The default is false.
+  final bool toggleable;
+
+  @override
+  bool get tristate => toggleable;
+
+  /// Called when the user selects this radio button.
+  ///
+  /// The radio button passes [value] as a parameter to this callback. The radio
+  /// button does not actually change state until the parent widget rebuilds the
+  /// radio button with the new [groupValue].
+  ///
+  /// If null, the radio button will be displayed as disabled.
+  ///
+  /// The provided callback will not be invoked if this radio button is already
+  /// selected.
+  ///
+  /// The callback provided to [onChanged] should update the state of the parent
+  /// [StatefulWidget] using the [State.setState] method, so that the parent
+  /// gets rebuilt; for example:
+  ///
+  /// ```dart
+  /// YaruRadio<SingingCharacter>(
+  ///   value: SingingCharacter.lafayette,
+  ///   groupValue: _character,
+  ///   onChanged: (SingingCharacter newValue) {
+  ///     setState(() {
+  ///       _character = newValue;
+  ///     });
+  ///   },
+  /// )
+  /// ```
+  @override
+  final ValueChanged<T?>? onChanged;
+
+  @override
+  bool get interactive => onChanged != null;
+
+  /// {@macro flutter.widgets.Focus.focusNode}
+  @override
+  final FocusNode? focusNode;
+
+  /// {@macro flutter.widgets.Focus.autofocus}
+  @override
+  final bool autofocus;
 
   @override
   YaruTogglableState<YaruRadio<T?>> createState() {
