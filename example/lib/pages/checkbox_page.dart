7d00ca3806053edccf976f622b307bac16ec8e46
J-P Nurmi <jp.nurmi@canonical.com>
2022-12-04 21:46:51 +0100

    Example: reorganize checkboxes, radios, and switches

---
+++ example/lib/pages/checkbox_page.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:yaru_widgets/yaru_widgets.dart';
+
+class CheckboxPage extends StatefulWidget {
+  const CheckboxPage({super.key});
+
+  @override
+  _CheckboxPageState createState() => _CheckboxPageState();
+}
+
+class _CheckboxPageState extends State<CheckboxPage> {
+  final List<bool?> _checkboxValues = [false, null, true];
+  final List<bool?> _buttonValues = [false, null, true];
+  final List<bool?> _listTileValues = [false, null, true];
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      padding: const EdgeInsets.all(kYaruPagePadding),
+      children: [
+        for (var i = 0; i < _checkboxValues.length; ++i) ...[
+          Row(
+            children: [
+              YaruCheckbox(
+                value: _checkboxValues[i],
+                onChanged: (v) => setState(() => _checkboxValues[i] = v),
+                tristate: true,
+              ),
+              const SizedBox(width: 10),
+              YaruCheckbox(
+                value: _checkboxValues[i],
+                onChanged: null,
+                tristate: true,
+              )
+            ],
+          ),
+          const SizedBox(height: 10),
+        ],
+        const Divider(),
+        for (var i = 0; i < _buttonValues.length; ++i) ...[
+          YaruCheckButton(
+            title: const Text('YaruCheckButton'),
+            value: _buttonValues[i],
+            onChanged: (v) => setState(() => _buttonValues[i] = v),
+            tristate: true,
+          ),
+          const SizedBox(height: 10),
+        ],
+        const Divider(),
+        for (var i = 0; i < _listTileValues.length; ++i) ...[
+          YaruCheckboxListTile(
+            value: _listTileValues[i],
+            onChanged: (v) => setState(() => _listTileValues[i] = v),
+            tristate: true,
+            title: const Text('YaruCheckboxListTile'),
+          ),
+          const SizedBox(height: 10),
+        ],
+      ],
+    );
+  }
+}
