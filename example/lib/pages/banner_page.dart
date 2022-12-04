1ea217f5d6d2c66d005afd490b8254dc6dc529bd
Frederik Feichtmeier <frederik.feichtmeier@gmail.com>
2022-12-04 11:36:30 +0100

    YaruBanner: forward onHover to .tile constructor (#446)
    * YaruBanner: forward onHover to .tile constructor
    
    * Show onHover in example

--- example/lib/pages/banner_page.dart
+++ example/lib/pages/banner_page.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:yaru_icons/yaru_icons.dart';
 import 'package:yaru_widgets/yaru_widgets.dart';
 
+const _lorem =
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+
 class BannerPage extends StatelessWidget {
   const BannerPage({super.key});
 
@@ -23,25 +26,74 @@ class BannerPage extends StatelessWidget {
               Icons.cloud,
               size: 100,
             ),
-            child: YaruBanner.tile(
-              title: Text('YaruBanner $i'),
-              subtitle: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const Text('Description'),
-                  if (i.isEven) const Text('Third line'),
-                ],
-              ),
-              icon: Icon(
-                YaruIcons.weather_clear,
-                size: 80,
-                color: Theme.of(context).primaryColor,
-              ),
-              onTap: () => showAboutDialog(context: context),
-              surfaceTintColor: i.isEven ? Colors.pink : null,
-            ),
+            child: _Banner(i: i),
           )
       ],
+    );
+  }
+}
+
+class _Banner extends StatefulWidget {
+  const _Banner({
+    Key? key,
+    required this.i,
+  }) : super(key: key);
+
+  final int i;
+
+  @override
+  State<_Banner> createState() => _BannerState();
+}
+
+class _BannerState extends State<_Banner> {
+  var _hovered = false;
+
+  @override
+  Widget build(BuildContext context) {
+    final title = Text('YaruBanner ${widget.i}');
+    final description = _hovered
+        ? const SizedBox(width: 200, height: 100, child: Text(_lorem))
+        : const Text(
+            'Description',
+            overflow: TextOverflow.ellipsis,
+          );
+    final thirdLine = _hovered ? null : const Text('Third line');
+    final icon = _hovered
+        ? null
+        : Icon(
+            YaruIcons.weather_clear,
+            size: 80,
+            color: Theme.of(context).primaryColor,
+          );
+
+    return YaruBanner.tile(
+      onHover: (hovered) => setState(() => _hovered = hovered),
+      title: title,
+      subtitle: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          description,
+          if (thirdLine != null) thirdLine,
+        ],
+      ),
+      icon: icon,
+      onTap: () => showDialog(
+        context: context,
+        builder: (context) => SimpleDialog(
+          titlePadding: EdgeInsets.zero,
+          contentPadding: const EdgeInsets.all(10),
+          title: YaruTitleBar(
+            title: title,
+          ),
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: description,
+            )
+          ],
+        ),
+      ),
+      surfaceTintColor: widget.i.isEven ? Colors.pink : null,
     );
   }
 }
