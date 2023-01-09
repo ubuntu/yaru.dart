837f1d5f7946f549efb72e6e2dea8841e7f74895
Dennis Loose <dennis.loose@canonical.com>
2023-01-09 17:45:55 +0100

    add NavigationPage

---
+++ example/lib/pages/navigation_page.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:yaru_widgets/yaru_widgets.dart';
+
+class NavigationPage extends StatelessWidget {
+  const NavigationPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(kYaruPagePadding),
+      child: OutlinedButton(
+        onPressed: () => Navigator.of(context)
+            .push(MaterialPageRoute(builder: (_) => _PageTwo())),
+        child: const Text('next page'),
+      ),
+    );
+  }
+}
+
+class _PageTwo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Page 2')),
+      body: Padding(
+        padding: const EdgeInsets.all(kYaruPagePadding),
+        child: OutlinedButton(
+          onPressed: () => Navigator.of(context)
+              .push(MaterialPageRoute(builder: (_) => _PageThree())),
+          child: const Text('next page'),
+        ),
+      ),
+    );
+  }
+}
+
+class _PageThree extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Page 3')),
+      body: const Padding(
+        padding: EdgeInsets.all(kYaruPagePadding),
+        child: Text('this is the last page'),
+      ),
+    );
+  }
+}
