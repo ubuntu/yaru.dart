ubuntu/yaru.dart

commit 7030f21454c670e675da39ea8424ca60650419da

YaruWindowTitleBar: fix slow window state init (#506)
Fixes: #505

lib/src/controls/yaru_title_bar.dart
@@ -352,14 +352,15 @@ class YaruWindowTitleBar extends StatelessWidget
   @override
   Widget build(BuildContext context) {
     if (isVisible == false) return const SizedBox.shrink();
+    final windowState = YaruWindow.state(context);
     return StreamBuilder<YaruWindowState>(
-      stream: YaruWindow.states(),
+      stream: YaruWindow.states(context),
       initialData: YaruWindowState(
-        isActive: isActive,
-        isClosable: isClosable,
-        isMaximizable: isMaximizable,
-        isMinimizable: isMinimizable,
-        isRestorable: isRestorable,
+        isActive: isActive ?? windowState?.isActive,
+        isClosable: isClosable ?? windowState?.isClosable,
+        isMaximizable: isMaximizable ?? windowState?.isMaximizable,
+        isMinimizable: isMinimizable ?? windowState?.isMinimizable,
+        isRestorable: isRestorable ?? windowState?.isRestorable,
       ),
       builder: (context, snapshot) {
         final window = snapshot.data;

lib/src/controls/yaru_window.dart
@@ -7,24 +7,43 @@ import 'package:flutter/widgets.dart';
 import 'package:window_manager/window_manager.dart';
 
 class YaruWindow {
-  @visibleForTesting
-  static WindowManager wm = WindowManager.instance;
+  static final Map<Object, YaruWindowInstance> _windows = {};
 
-  static Future<void> close(_) => wm.close().catchError((_) {});
-  static Future<void> drag(_) => wm.startDragging().catchError((_) {});
-  static Future<void> maximize(_) => wm.maximize().catchError((_) {});
-  static Future<void> minimize(_) => wm.minimize().catchError((_) {});
-  static Future<void> restore(_) => wm.unmaximize().catchError((_) {});
-  static Future<void> showMenu(_) => wm.popUpWindowMenu().catchError((_) {});
-  static Future<YaruWindowState> state() => wm.state();
-  static Stream<YaruWindowState> states() async* {
-    final listener = YaruWindowListener(wm);
-    yield await wm.state();
-    try {
-      yield* listener.listen();
-    } finally {
-      await listener.close();
-    }
+  static YaruWindowInstance of(BuildContext context) {
+    const id = 0; // View.of(context).windowId;
+    return _windows[id] ??= YaruWindowInstance._(id);
+  }
+
+  static Future<void> close(BuildContext context) {
+    return YaruWindow.of(context).close();
+  }
+
+  static Future<void> drag(BuildContext context) {
+    return YaruWindow.of(context).drag();
+  }
+
+  static Future<void> maximize(BuildContext context) {
+    return YaruWindow.of(context).maximize();
+  }
+
+  static Future<void> minimize(BuildContext context) {
+    return YaruWindow.of(context).minimize();
+  }
+
+  static Future<void> restore(BuildContext context) {
+    return YaruWindow.of(context).restore();
+  }
+
+  static Future<void> showMenu(BuildContext context) {
+    return YaruWindow.of(context).showMenu();
+  }
+
+  static YaruWindowState? state(BuildContext context) {
+    return YaruWindow.of(context).state;
+  }
+
+  static Stream<YaruWindowState> states(BuildContext context) {
+    return YaruWindow.of(context).states();
   }
 
   static Future<void> maybePop(BuildContext context) {
@@ -34,10 +53,30 @@ class YaruWindow {
   static Future<void> ensureInitialized() async {
     WidgetsFlutterBinding.ensureInitialized();
     if (!kIsWeb) {
-      await wm.ensureInitialized();
-      await wm.setTitleBarStyle(TitleBarStyle.hidden);
+      await windowManager.ensureInitialized();
+      await windowManager.setTitleBarStyle(TitleBarStyle.hidden);
     }
   }
+}
+
+class YaruWindowInstance {
+  YaruWindowInstance._(this._id);
+
+  final Object _id; // ignore: unused_field
+  final _listener = YaruWindowListener(wm);
+
+  @visibleForTesting
+  static WindowManager wm = WindowManager.instance;
+
+  Future<void> close() => wm.close().catchError((_) {});
+  Future<void> drag() => wm.startDragging().catchError((_) {});
+  Future<void> maximize() => wm.maximize().catchError((_) {});
+  Future<void> minimize() => wm.minimize().catchError((_) {});
+  Future<void> restore() => wm.unmaximize().catchError((_) {});
+  Future<void> showMenu() => wm.popUpWindowMenu().catchError((_) {});
+
+  YaruWindowState? get state => _listener.state;
+  Stream<YaruWindowState> states() => _listener.states();
 }
 
 extension YaruWindowManagerX on WindowManager {
@@ -94,36 +133,42 @@ class YaruWindowListener implements WindowListener {
   YaruWindowListener(this._wm);
 
   final WindowManager _wm;
-  final _controller = StreamController<YaruWindowState>();
+  StreamController<YaruWindowState>? _controller;
+  YaruWindowState? _state;
 
-  Stream<YaruWindowState> listen() {
-    _wm.addListener(this);
-    return _controller.stream;
+  YaruWindowState? get state => _state;
+
+  Stream<YaruWindowState> states() {
+    _controller ??= StreamController<YaruWindowState>.broadcast(
+      onListen: () => _wm.addListener(this),
+      onCancel: () => _wm.removeListener(this),
+    );
+    return _controller!.stream;
   }
 
-  Future<void> close() async {
-    _wm.removeListener(this);
-    await _controller.close();
+  Future<void> close() async => await _controller?.close();
+
+  Future<void> _updateState() async {
+    _state = await _wm.state();
+    _controller?.add(_state!);
   }
 
-  Future<void> _emitState() async => _controller.add(await _wm.state());
-
   @override
-  void onWindowBlur() => _emitState();
+  void onWindowBlur() => _updateState();
   @override
-  void onWindowFocus() => _emitState();
+  void onWindowFocus() => _updateState();
   @override
-  void onWindowEnterFullScreen() => _emitState();
+  void onWindowEnterFullScreen() => _updateState();
   @override
-  void onWindowLeaveFullScreen() => _emitState();
+  void onWindowLeaveFullScreen() => _updateState();
   @override
-  void onWindowMaximize() => _emitState();
+  void onWindowMaximize() => _updateState();
   @override
-  void onWindowUnmaximize() => _emitState();
+  void onWindowUnmaximize() => _updateState();
   @override
-  void onWindowMinimize() => _emitState();
+  void onWindowMinimize() => _updateState();
   @override
-  void onWindowRestore() => _emitState();
+  void onWindowRestore() => _updateState();
   @override
   void onWindowClose() {}
   @override

commit b4781cb462633512132ff61d7da2b1b1a25671ef

YaruWindowTitleBar: fix state init
This fixes a regression introduced by #506 that e.g. the settings
dialog title bar in the example is not draggable.

lib/src/controls/yaru_title_bar.dart
@@ -352,33 +352,33 @@ class YaruWindowTitleBar extends StatelessWidget
   @override
   Widget build(BuildContext context) {
     if (isVisible == false) return const SizedBox.shrink();
-    final windowState = YaruWindow.state(context);
+    final defaultState = YaruWindowState(
+      isActive: isActive,
+      isClosable: isClosable,
+      isMaximizable: isMaximizable,
+      isMinimizable: isMinimizable,
+      isRestorable: isRestorable,
+    );
     return StreamBuilder<YaruWindowState>(
       stream: YaruWindow.states(context),
-      initialData: YaruWindowState(
-        isActive: isActive ?? windowState?.isActive,
-        isClosable: isClosable ?? windowState?.isClosable,
-        isMaximizable: isMaximizable ?? windowState?.isMaximizable,
-        isMinimizable: isMinimizable ?? windowState?.isMinimizable,
-        isRestorable: isRestorable ?? windowState?.isRestorable,
-      ),
+      initialData: YaruWindow.state(context),
       builder: (context, snapshot) {
-        final window = snapshot.data;
+        final state = snapshot.data?.merge(defaultState) ?? defaultState;
         return YaruTitleBar(
           leading: leading,
-          title: title ?? Text(window?.title ?? ''),
+          title: title ?? Text(state.title ?? ''),
           trailing: trailing,
           centerTitle: centerTitle,
           titleSpacing: titleSpacing,
           backgroundColor: backgroundColor,
           shape: shape,
           border: border,
-          isActive: isActive ?? window?.isActive,
-          isClosable: isClosable ?? window?.isClosable,
-          isDraggable: isDraggable ?? window?.isMovable,
-          isMaximizable: isMaximizable ?? window?.isMaximizable,
-          isMinimizable: isMinimizable ?? window?.isMinimizable,
-          isRestorable: isRestorable ?? window?.isRestorable,
+          isActive: state.isActive,
+          isClosable: state.isClosable,
+          isDraggable: state.isMovable,
+          isMaximizable: state.isMaximizable,
+          isMinimizable: state.isMinimizable,
+          isRestorable: state.isRestorable,
           onClose: onClose,
           onDrag: onDrag,
           onMaximize: onMaximize,

lib/src/controls/yaru_window.dart
@@ -138,12 +138,16 @@ class YaruWindowListener implements WindowListener {
 
   YaruWindowState? get state => _state;
 
-  Stream<YaruWindowState> states() {
+  Stream<YaruWindowState> states() async* {
     _controller ??= StreamController<YaruWindowState>.broadcast(
       onListen: () => _wm.addListener(this),
       onCancel: () => _wm.removeListener(this),
     );
-    return _controller!.stream;
+    if (_state == null) {
+      _state = await _wm.state();
+      yield _state!;
+    }
+    yield* _controller!.stream;
   }
 
   Future<void> close() async => await _controller?.close();
@@ -208,6 +212,47 @@ class YaruWindowState {
   final bool? isMovable;
   final bool? isRestorable;
   final String? title;
+
+  YaruWindowState copyWith({
+    bool? isActive,
+    bool? isClosable,
+    bool? isFullscreen,
+    bool? isMaximizable,
+    bool? isMaximized,
+    bool? isMinimizable,
+    bool? isMinimized,
+    bool? isMovable,
+    bool? isRestorable,
+    String? title,
+  }) {
+    return YaruWindowState(
+      isActive: isActive ?? this.isActive,
+      isClosable: isClosable ?? this.isClosable,
+      isFullscreen: isFullscreen ?? this.isFullscreen,
+      isMaximizable: isMaximizable ?? this.isMaximizable,
+      isMaximized: isMaximized ?? this.isMaximized,
+      isMinimizable: isMinimizable ?? this.isMinimizable,
+      isMinimized: isMinimized ?? this.isMinimized,
+      isMovable: isMovable ?? this.isMovable,
+      isRestorable: isRestorable ?? this.isRestorable,
+      title: title ?? this.title,
+    );
+  }
+
+  YaruWindowState merge(YaruWindowState? other) {
+    return copyWith(
+      isActive: other?.isActive,
+      isClosable: other?.isClosable,
+      isFullscreen: other?.isFullscreen,
+      isMaximizable: other?.isMaximizable,
+      isMaximized: other?.isMaximized,
+      isMinimizable: other?.isMinimizable,
+      isMinimized: other?.isMinimized,
+      isMovable: other?.isMovable,
+      isRestorable: other?.isRestorable,
+      title: other?.title,
+    );
+  }
 
   @override
   bool operator ==(Object other) {