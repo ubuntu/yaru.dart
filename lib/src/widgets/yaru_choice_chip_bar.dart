055b7c943f9c063aef70ca9030fe65537c184f14
Feichtmeier <frederik.feichtmeier@gmail.com>
2023-06-22 18:24:00 +0200

    feat: improve YaruChipChoice parameters

--- lib/src/widgets/yaru_choice_chip_bar.dart
+++ lib/src/widgets/yaru_choice_chip_bar.dart
@@ -1,29 +1,93 @@
 import 'package:flutter/material.dart';
 import 'package:yaru_icons/yaru_icons.dart';
 
+/// A list of [ChoiceChipBar]s wrapped either in a controllable [ListView] or [Wrap].
 class YaruChoiceChipBar extends StatefulWidget {
   const YaruChoiceChipBar({
     super.key,
-    this.wrapScrollDirection = Axis.horizontal,
     required this.labels,
-    required this.onSelected,
+    this.onSelected,
     required this.isSelected,
     this.yaruChoiceChipBarStyle = YaruChoiceChipBarStyle.row,
     this.spacing = 10.0,
-    this.duration = const Duration(milliseconds: 200),
-    this.animationStep = 100.0,
+    this.animationDuration = const Duration(milliseconds: 300),
+    this.navigationStep = 100.0,
+    this.animationCurve = Curves.bounceIn,
+    this.radius = 34.0,
+    this.chipHeight = 34.0,
+    this.wrapScrollDirection = Axis.horizontal,
+    this.wrapAlignment = WrapAlignment.start,
+    this.wrapRunAlignment = WrapAlignment.start,
+    this.wrapRunSpacing = 10.0,
+    this.wrapCrossAlignment = WrapCrossAlignment.start,
+    this.wrapVerticalDirection = VerticalDirection.down,
+    this.wrapClipBehavior = Clip.none,
+    this.wrapTextDirection,
   }) : assert(labels.length == isSelected.length);
 
-  final Axis wrapScrollDirection;
-  final YaruChoiceChipBarStyle yaruChoiceChipBarStyle;
-  final double spacing;
-  final Duration duration;
-  final double animationStep;
-
+  /// The [List] of [Widget]'s used to generate a [List] of [ChoiceChip]s
+  /// in which they are placed.
+  /// The length of [labels] mus be equal to [isSelected]!
   final List<Widget> labels;
+
+  /// The [List] of [bool] used to determine if the assigned
+  /// [ChoiceChip] is selected.
+  /// The length of [isSelected] must be equal to [labels]!
   final List<bool> isSelected;
 
-  final void Function(int index) onSelected;
+  /// The [Function] called when a [ChoiceChip] with given `index` is selected.
+  /// If [onSelected] is null, the whole [YaruChoiceChipBar] is disabled.
+  final void Function(int index)? onSelected;
+
+  /// Determines weither the [ChoiceChip]s should be put into a [ListView]
+  /// or a [Wrap] or a [ListView] but with the scrolling controls
+  /// put into a [Stack] on top of the [ListView].
+  final YaruChoiceChipBarStyle yaruChoiceChipBarStyle;
+
+  /// Sets how long the navigation jumps and fade in and out of
+  /// the scrolling controls are animated.
+  final Duration animationDuration;
+
+  /// Sets how far each scrolling step jumps in the [ListView].
+  final double navigationStep;
+
+  /// Sets the easing [Curve] of the animations.
+  final Curve animationCurve;
+
+  /// Sets how round the [ChoiceChips] and scrolling control buttons are.
+  final double radius;
+
+  /// Sets how high the whole bar is.
+  final double chipHeight;
+
+  /// The spacing of [ChoiceChip]s inside the [Wrap] or the [ListView]
+  final double spacing;
+
+  /// The spacing of the [ChoiceChip]s when the [Wrap] places them
+  ///  in another row.
+  final double wrapRunSpacing;
+
+  /// The alignment of the [ChoiceChip]s when the [Wrap] places them
+  /// in another row.
+  final Axis wrapScrollDirection;
+
+  /// The [WrapAlignment] of the [ChoiceChip]s with `YaruChoiceChipStyle.wrap`.
+  final WrapAlignment wrapAlignment;
+
+  /// The [WrapCrossAlignment] of the [ChoiceChip]s with `YaruChoiceChipStyle.wrap`
+  final WrapCrossAlignment wrapCrossAlignment;
+
+  ///  of the [ChoiceChip]s with `YaruChoiceChipStyle.wrap`
+  final Clip wrapClipBehavior;
+
+  /// The [WrapAlignment] of the [ChoiceChip]s with with `YaruChoiceChipStyle.wrap`
+  final WrapAlignment wrapRunAlignment;
+
+  /// The [TextDirection] of the [ChoiceChip]s with with `YaruChoiceChipStyle.wrap`
+  final TextDirection? wrapTextDirection;
+
+  /// The [VerticalDirection] of the [ChoiceChip]s with `YaruChoiceChipStyle.wrap`
+  final VerticalDirection wrapVerticalDirection;
 
   @override
   State<YaruChoiceChipBar> createState() => _YaruChoiceChipBarState();
@@ -68,21 +132,34 @@ class _YaruChoiceChipBarState extends State<YaruChoiceChipBar> {
 
   @override
   Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
+    Widget themedChip(int index) {
+      return ChipTheme(
+        data: theme.chipTheme.copyWith(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(widget.radius),
+            side: BorderSide(
+              color: theme.colorScheme.outline,
+              width: 1,
+            ),
+          ),
+        ),
+        child: ChoiceChip(
+          label: widget.labels[index],
+          selected: widget.isSelected[index],
+          onSelected: widget.onSelected == null
+              ? null
+              : (v) => widget.onSelected!(index),
+        ),
+      );
+    }
+
     final children = [
       for (int index = 0; index < widget.labels.length; index++)
-        if (widget.isSelected[index])
-          ChoiceChip(
-            label: widget.labels[index],
-            selected: widget.isSelected[index],
-            onSelected: (v) => widget.onSelected(index),
-          ),
+        if (widget.isSelected[index]) themedChip(index),
       for (int index = 0; index < widget.labels.length; index++)
-        if (!widget.isSelected[index])
-          ChoiceChip(
-            label: widget.labels[index],
-            selected: widget.isSelected[index],
-            onSelected: (v) => widget.onSelected(index),
-          )
+        if (!widget.isSelected[index]) themedChip(index)
     ];
 
     final listView = ListView(
@@ -101,31 +178,39 @@ class _YaruChoiceChipBarState extends State<YaruChoiceChipBar> {
     );
 
     final goPreviousButton = _NavigationButton(
+      radius: widget.radius,
+      chipHeight: widget.chipHeight,
       icon: const Icon(YaruIcons.go_previous),
       onTap: _enableGoPreviousButton
           ? () => _controller.animateTo(
-                _controller.position.pixels - widget.animationStep,
-                duration: widget.duration,
-                curve: Curves.bounceIn,
+                _controller.position.pixels - widget.navigationStep,
+                duration: widget.animationDuration,
+                curve: widget.animationCurve,
               )
           : null,
     );
 
     final goNextButton = _NavigationButton(
+      chipHeight: widget.chipHeight,
+      radius: widget.radius,
       icon: const Icon(YaruIcons.go_next),
       onTap: _enableGoNextButton
           ? () => _controller.animateTo(
-                _controller.position.pixels + widget.animationStep,
-                duration: widget.duration,
-                curve: Curves.bounceIn,
+                _controller.position.pixels + widget.navigationStep,
+                duration: widget.animationDuration,
+                curve: widget.animationCurve,
               )
           : null,
     );
 
-    const clipRadius = 40.0;
-
     if (widget.yaruChoiceChipBarStyle == YaruChoiceChipBarStyle.wrap) {
       return Wrap(
+        alignment: widget.wrapAlignment,
+        clipBehavior: widget.wrapClipBehavior,
+        crossAxisAlignment: widget.wrapCrossAlignment,
+        runAlignment: widget.wrapRunAlignment,
+        textDirection: widget.wrapTextDirection,
+        verticalDirection: widget.wrapVerticalDirection,
         spacing: widget.spacing,
         runSpacing: widget.spacing,
         direction: widget.wrapScrollDirection,
@@ -133,27 +218,31 @@ class _YaruChoiceChipBarState extends State<YaruChoiceChipBar> {
       );
     } else if (widget.yaruChoiceChipBarStyle == YaruChoiceChipBarStyle.stack) {
       return SizedBox(
-        height: 60,
+        height: widget.chipHeight,
         child: Stack(
           alignment: Alignment.center,
           children: [
             ClipRRect(
               borderRadius: BorderRadius.only(
-                topLeft:
-                    Radius.circular(_enableGoPreviousButton ? clipRadius : 0.0),
-                bottomLeft:
-                    Radius.circular(_enableGoPreviousButton ? clipRadius : 0.0),
-                topRight:
-                    Radius.circular(_enableGoNextButton ? clipRadius : 0.0),
-                bottomRight:
-                    Radius.circular(_enableGoNextButton ? clipRadius : 0.0),
+                topLeft: Radius.circular(
+                  _enableGoPreviousButton ? widget.chipHeight : 0.0,
+                ),
+                bottomLeft: Radius.circular(
+                  _enableGoPreviousButton ? widget.chipHeight : 0.0,
+                ),
+                topRight: Radius.circular(
+                  _enableGoNextButton ? widget.chipHeight : 0.0,
+                ),
+                bottomRight: Radius.circular(
+                  _enableGoNextButton ? widget.chipHeight : 0.0,
+                ),
               ),
               child: listView,
             ),
             Positioned(
               left: 0,
               child: AnimatedOpacity(
-                duration: const Duration(milliseconds: 300),
+                duration: widget.animationDuration,
                 opacity: _enableGoPreviousButton ? 1.0 : 0.0,
                 child: goPreviousButton,
               ),
@@ -161,7 +250,7 @@ class _YaruChoiceChipBarState extends State<YaruChoiceChipBar> {
             Positioned(
               right: 0,
               child: AnimatedOpacity(
-                duration: const Duration(milliseconds: 300),
+                duration: widget.animationDuration,
                 opacity: _enableGoNextButton ? 1.0 : 0.0,
                 child: goNextButton,
               ),
@@ -171,7 +260,7 @@ class _YaruChoiceChipBarState extends State<YaruChoiceChipBar> {
       );
     } else {
       return SizedBox(
-        height: 60,
+        height: widget.chipHeight,
         child: Row(
           children: [
             goPreviousButton,
@@ -196,25 +285,29 @@ class _NavigationButton extends StatelessWidget {
   const _NavigationButton({
     this.onTap,
     required this.icon,
+    required this.radius,
+    required this.chipHeight,
   });
 
   final Function()? onTap;
   final Widget icon;
+  final double radius;
+  final double chipHeight;
 
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
-    const navigationButtonSize = 34.0;
     final roundedRectangleBorder = RoundedRectangleBorder(
-      borderRadius: BorderRadius.circular(8),
+      borderRadius: BorderRadius.circular(radius),
       side: BorderSide(
         color: theme.colorScheme.outline,
         width: 1,
       ),
     );
+
     return SizedBox(
-      height: navigationButtonSize,
-      width: navigationButtonSize,
+      height: chipHeight,
+      width: chipHeight,
       child: Material(
         shape: roundedRectangleBorder,
         child: InkWell(
