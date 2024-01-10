8031b46739d4b854553200bbf5dcfbbc4fd0c366
Paul Kepinski <paul.kepinski@orange.fr>
2024-01-10 19:22:39 +0100

    Validate entry input in example

--- example/lib/pages/date_time_entry_page.dart
+++ example/lib/pages/date_time_entry_page.dart
@@ -11,27 +11,33 @@ class DateTimePage extends StatefulWidget {
 class _DateTimePageState extends State<DateTimePage> {
   DateTime? _dateTime;
 
+  final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Center(
       child: SizedBox(
         width: 250,
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            YaruDateTimeEntry(
-              initialDate: DateTime.now(),
-              firstDate: DateTime(1900),
-              lastDate: DateTime(2050),
-              onChanged: (dateTime) {
-                setState(() {
-                  _dateTime = dateTime;
-                });
-              },
-            ),
-            const SizedBox(height: 25),
-            Text(_dateTime.toString()),
-          ],
+        child: Form(
+          key: _formKey,
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              YaruDateTimeEntry(
+                initialDate: DateTime.now(),
+                firstDate: DateTime(1900),
+                lastDate: DateTime(2050),
+                onChanged: (dateTime) {
+                  setState(() {
+                    _formKey.currentState?.validate();
+                    _dateTime = dateTime;
+                  });
+                },
+              ),
+              const SizedBox(height: 25),
+              Text(_dateTime.toString()),
+            ],
+          ),
         ),
       ),
     );
