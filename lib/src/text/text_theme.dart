480ee34f5e37ee2f264ae62fd5307ff4b9ad3070
Feichtmeier <frederik.feichtmeier@gmail.com>
2024-02-24 11:16:49 +0100

    fix: use the ubuntu font

--- lib/src/text/text_theme.dart
+++ lib/src/text/text_theme.dart
@@ -87,7 +87,8 @@ class _UbuntuTextStyle extends TextStyle {
     required this.textColor,
   }) : super(
           fontFamily: 'Ubuntu',
-          package: 'yaru',
+          // TODO: careful not not miss this when we migrate the name to yaru again
+          package: 'yaru_widgets',
           color: textColor,
         );
   final Color textColor;
