354d98c0f35b962cd965b7ca89b2fed38a19dec4
Paul Kepinski <paul.kepinski@orange.fr>
2024-12-03 20:32:48 +0100

    Formatting

--- lib/src/widgets/yaru_progress_indicator.dart
+++ lib/src/widgets/yaru_progress_indicator.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+
 abstract class YaruProgressIndicator extends StatefulWidget {
   /// Creates a Yaru progress indicator.
   ///
