b5d5b0e0ed167ff0696701686a7568f06c831889
ashuntu <ashton.nelson@canonical.com>
2025-08-05 15:05:56 -0500

    Add on/off shape tests and update goldens

--- test/widgets/yaru_switch_list_tile_test.dart
+++ test/widgets/yaru_switch_list_tile_test.dart
@@ -107,6 +107,48 @@ void main() {
     variant: goldenVariant,
     tags: 'golden',
   );
+
+  testWidgets(
+    'golden images (with shapes)',
+    (tester) async {
+      final variant = goldenVariant.currentValue!;
+
+      // ensure traditional focus highlight
+      FocusManager.instance.highlightStrategy =
+          FocusHighlightStrategy.alwaysTraditional;
+
+      await tester.pumpScaffold(
+        YaruSwitchListTile(
+          autofocus: variant.hasState(WidgetState.focused),
+          value: variant.hasState(WidgetState.selected),
+          onChanged: variant.hasState(WidgetState.disabled) ? null : (_) {},
+          title: const Text('YaruSwitchListTile'),
+          subtitle: const Text('Lorem ipsum dolor sit amet'),
+          onOffShapes: true,
+        ),
+        themeMode: variant.themeMode,
+        size: const Size(325, 72),
+      );
+      await tester.pumpAndSettle();
+
+      if (variant.hasState(WidgetState.pressed)) {
+        await tester.down(find.byType(YaruSwitch));
+        await tester.pumpAndSettle();
+      } else if (variant.hasState(WidgetState.hovered)) {
+        await tester.hover(find.byType(YaruSwitch));
+        await tester.pumpAndSettle();
+      }
+
+      await expectLater(
+        find.byType(YaruSwitchListTile),
+        matchesGoldenFile(
+          'goldens/yaru_switch_list_tile-${variant.label}-shapes.png',
+        ),
+      );
+    },
+    variant: goldenVariant,
+    tags: 'golden',
+  );
 }
 
 final goldenVariant = ValueVariant({
