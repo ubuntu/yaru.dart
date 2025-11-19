5b022d142830519c816ea72dd4a290a3980f2dd9
ashuntu <ashton.nelson@canonical.com>
2025-11-19 16:02:09 -0600

    fix: Remove unnecessary import

--- lib/src/widgets/yaru_carousel.dart
+++ lib/src/widgets/yaru_carousel.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
-import 'package:yaru/icons.dart';
 // ignore: unnecessary_import
 import 'package:yaru/widgets.dart';
 import 'package:yaru/yaru.dart';
